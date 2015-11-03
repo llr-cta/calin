246be6056ae2bee03f47ae271867ed5c56fd8e9d
Stephen Fegan <sfegan@gmail.com>
2015-11-03 13:26:38 +0100

    More copyright headers
    [ci skip]

--- include/calib/pmt_model_pg.hpp
+++ include/calib/pmt_model_pg.hpp
@@ -2,6 +2,23 @@
 
 // PMTModelPG.hpp - Stephen Fegan - sfegan@llr.in2p3.fr - December 2013
 
+/* 
+
+   Copyright 2015, Stephen Fegan <sfegan@gmail.com>
+
+   This file is part of "calin"
+   
+   "calin" is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License version 2 or
+   later, as published by the Free Software Foundation.
+    
+   "calin" is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+*/
+
 #include<vector>
 
 namespace calin { namespace calib { namespace pmt_model_pg {
