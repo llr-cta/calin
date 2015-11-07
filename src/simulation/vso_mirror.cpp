6392e964ac6bcd5e877c6075ba6e0c527e63a1df
Stephen Fegan <sfegan@gmail.com>
2015-11-07 14:39:38 +0100

    Copyright header
    [ci skip]

--- src/simulation/vso_mirror.cpp
+++ src/simulation/vso_mirror.cpp
@@ -1,6 +1,26 @@
-//-*-mode:c++; mode:font-lock;-*-
+/* 
 
+   calin/simulation/vso_mirror.cpp -- Stephen Fegan -- 2015-11-05
 
+   Class for mirror data and functionality. This code is derived from
+   raytracing code largely implemented by the author at UCLA in
+   2004. It is not complient with the calin coding style.
+
+   Copyright 2015, Stephen Fegan <sfegan@llr.in2p3.fr>
+   LLR, Ecole polytechnique, CNRS/IN2P3, Universite Paris-Saclay
+
+   This file is part of "calin"
+   
+   "calin" is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License version 2 or
+   later, as published by the Free Software Foundation.
+    
+   "calin" is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+*/
 
 #include <sstream>
 #include <cassert>
