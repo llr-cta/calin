2e35ff1946be199b23a6602a0a1ec865fba61fc8
Stephen Fegan <sfegan@gmail.com>
2015-12-05 15:15:35 +0100

    Small steps towards PB->Swig

--- tools/protoc-gen-swig/swig_generator.cpp
+++ tools/protoc-gen-swig/swig_generator.cpp
@@ -37,14 +37,85 @@ SwigGenerator::~SwigGenerator()
 
 namespace {
 
-string pb_to_i_filename(string pb_filename)
+string pb_to_gen_filename(string pb_filename, string extension = ".pb.i")
 {
-  return pb_filename.substr(0,pb_filename.find_last_of('.')) + ".pb.i";
+  return pb_filename.substr(0,pb_filename.find_last_of('.')) + extension;
 }
 
-string pb_to_h_filename(string pb_filename)
+void print_includes(Printer* I, const google::protobuf::FileDescriptor * file,
+                    string directive = "#include", string extension = ".pb.h")
 {
-  return pb_filename.substr(0,pb_filename.find_last_of('.')) + ".pb.h";
+  for(int i=0; i<file->dependency_count(); i++)
+    I->Print("$directive$<$file$>\n","directive", directive, "file",
+             pb_to_gen_filename(file->dependency(i)->name(),extension));
+  for(int i=0; i<file->public_dependency_count(); i++)
+    I->Print("$directive$<$file$>\n","directive", directive, "file",
+             pb_to_gen_filename(file->public_dependency(i)->name(),extension));
+  for(int i=0; i<file->weak_dependency_count(); i++) 
+    I->Print("$directive$<$file$>\n","directive", directive, "file",
+             pb_to_gen_filename(file->weak_dependency(i)->name(),extension));
+}
+
+void print_message(Printer* I, const google::protobuf::Descriptor* d)
+{
+  I->Print("class $name$\n"
+           "{\n"
+           " public:\n",
+           "name", d->name());
+  I->Indent();
+
+  for(unsigned i=0;i<d->field_count();i++)
+  {
+    auto* f = d->field(i);
+    if(i>0)I->Print("\n");
+    if(f->message_type())
+    {
+
+    }
+    else if(f->is_map())
+    {
+
+    }
+#if 0
+    else if(f->is_enum())
+    {
+      
+    }
+#endif
+    else if(f->cpp_type_name() == std::string("string"))
+    {
+      if(f->is_repeated())
+        I->Print("int $name$_size() const;\n"
+                 "const std::string& $name$(int index) const;\n"
+                 "void add_$name$(const std::string& x);\n"
+                 "void clear_$name$();\n",
+                 "name", f->name());
+      else
+        I->Print("const std::string& $name$() const;\n"
+                 "void set_$name$(const std::string& x);\n"
+                 "void clear_$name$();\n",
+                 "name", f->name());
+    }
+    else
+    {
+      if(f->is_repeated())
+        I->Print("int $name$_size() const;\n"
+                 "$type$ $name$(int index) const;\n"
+                 "void add_$name$($type$ x);\n"
+                 "void clear_$name$();\n",
+                 "type", f->cpp_type_name(),
+                 "name", f->name());
+      else
+        I->Print("$type$ $name$() const;\n"
+                 "void set_$name$($type$ x);\n"
+                 "void clear_$name$();\n",
+                 "type", f->cpp_type_name(),
+                 "name", f->name());
+    }
+  }
+  
+  I->Outdent();
+  I->Print("};\n");
 }
 
 } // anonymous namespace
@@ -55,7 +126,7 @@ Generate(const google::protobuf::FileDescriptor * file,
          google::protobuf::compiler::GeneratorContext *context,
          string * error) const
 {
-  auto I_stream = context->Open(pb_to_i_filename(file->name()));
+  auto I_stream = context->Open(pb_to_gen_filename(file->name(),".pb.i"));
   Printer* I = new Printer(I_stream,'$');
 
   I->Print("// Auto-generated from \"$file$\". "
@@ -77,15 +148,7 @@ Generate(const google::protobuf::FileDescriptor * file,
 
   I->Print("\n%{\n");
   I->Indent();
-  for(int i=0; i<file->dependency_count(); i++)
-    I->Print("#include<$file$>\n","file",
-             pb_to_h_filename(file->dependency(i)->name()));
-  for(int i=0; i<file->public_dependency_count(); i++)
-    I->Print("#include<$file$>\n","file",
-             pb_to_h_filename(file->public_dependency(i)->name()));
-  for(int i=0; i<file->weak_dependency_count(); i++)
-    I->Print("#include<$file$>\n","file",
-             pb_to_h_filename(file->weak_dependency(i)->name()));
+  print_includes(I, file);
   I->Print("#define SWIG_FILE_WITH_INIT\n");
   I->Outdent();
   I->Print("%}\n\n");
@@ -96,17 +159,14 @@ Generate(const google::protobuf::FileDescriptor * file,
            "\n"
            "%include \"package_wide_definitions.i\"\n\n");
 
-  for(int i=0; i<file->dependency_count(); i++)
-    I->Print("#import \"$file$\"\n","file",
-             pb_to_i_filename(file->dependency(i)->name()));
-  for(int i=0; i<file->public_dependency_count(); i++)
-    I->Print("#import \"$file$\"\n","file",
-             pb_to_i_filename(file->public_dependency(i)->name()));
-  for(int i=0; i<file->weak_dependency_count(); i++)
-    I->Print("#import \"$file$\"\n","file",
-             pb_to_i_filename(file->weak_dependency(i)->name()));
-  
+  print_includes(I, file, "%import", ".pb.i");
 
+  for(int i=0;i<file->message_type_count();i++)
+  {
+    I->Print("\n");
+    print_message(I, file->message_type(i));
+  }
+    
   delete I;
   delete I_stream;
   return true;
