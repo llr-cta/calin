adf14467893e3e9ba6edcd5dff29dd0c23327b4c
Stephen Fegan <sfegan@gmail.com>
2016-02-01 06:58:37 +0100

    Add header files for event dispatcher/visitor
    [ci: skip]

---
+++ include/iact_data/event_dispatcher.hpp
@@ -0,0 +1,35 @@
+/*
+
+   calin/iact_data/event_dispatcher.hpp -- Stephen Fegan -- 2016-01-30
+
+   A dispatcher of run and event data
+
+   Copyright 2016, Stephen Fegan <sfegan@llr.in2p3.fr>
+   LLR, Ecole polytechnique, CNRS/IN2P3, Universite Paris-Saclay
+
+   This file is part of "calin"
+
+   "calin" is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License version 2 or
+   later, as published by the Free Software Foundation.
+
+   "calin" is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+*/
+
+#pragma once
+
+#include <string>
+
+#include <calin_global_definitions.hpp>
+#include <calin_global_config.hpp>
+#include <iact_data/telescope_data_source.hpp>
+#include <iact_data/event_visitor.hpp>
+
+namespace calin { namespace iact_data { namespace event_dispatcher {
+
+
+} } } // namespace calin::iact_data::event_dispatcher
---
+++ include/iact_data/event_visitor.hpp
@@ -0,0 +1,43 @@
+/*
+
+   calin/iact_data/event_visitor.hpp -- Stephen Fegan -- 2016-01-30
+
+   Vistor to event data
+
+   Copyright 2016, Stephen Fegan <sfegan@llr.in2p3.fr>
+   LLR, Ecole polytechnique, CNRS/IN2P3, Universite Paris-Saclay
+
+   This file is part of "calin"
+
+   "calin" is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License version 2 or
+   later, as published by the Free Software Foundation.
+
+   "calin" is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+*/
+
+#pragma once
+
+#include <string>
+
+#include <calin_global_definitions.hpp>
+#include <calin_global_config.hpp>
+#include <telescope_event.pb.h>
+
+namespace calin { namespace iact_data { namespace event_visitor {
+
+class TelescopeEventVisitor {
+  virtual ~TelescopeEventVisitor();
+
+  virtual bool visit_telescope_event(
+    ix::iact_data::telescope_event::TelescopeEvent* event);
+  virtual bool leave_telescope_event();
+
+  virtual bool visit_channel();
+};
+
+} } } // namespace calin::iact_data::event_visitor
