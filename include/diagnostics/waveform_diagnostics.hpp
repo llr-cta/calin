2691241efc1eb7c5bb320d3a499bc6be4d34c2d2
Stephen Fegan <sfegan@gmail.com>
2016-02-10 16:22:37 +0100

    Start waveform diagnostics

---
+++ include/diagnostics/waveform_diagnostics.hpp
@@ -0,0 +1,29 @@
+/*
+
+   calin/diagnostics/waveform_diagnostics.hpp -- Stephen Fegan -- 2016-02-10
+
+   Waveform diagnostics visitor
+
+   Copyright 2016, Stephen Fegan <sfegan@llr.in2p3.fr>
+   LLR, Ecole polytechnique, CNRS/IN2P3, Universite Paris-Saclay
+
+   This file is part of "calin"
+
+   "calin" is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License version 2 or
+   later, as published by the Free Software Foundation.
+
+   "calin" is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+*/
+
+#pragma once
+
+namespace calin { namespace diagnostics { namespace waveform_diagnostics {
+
+
+
+} } } /// namespace calin::diagnostics::waveform_diagnostics
