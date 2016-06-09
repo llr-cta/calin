333149b8e04ff5eb73126b0fd26e07338f1a44ad
Stephen Fegan <sfegan@gmail.com>
2016-06-09 12:37:01 +0200

    More specializations

--- include/math/covariance_calc.hpp
+++ include/math/covariance_calc.hpp
@@ -31,33 +31,43 @@ double cov_double_gen(double sij, int64_t nij,
   double si, int64_t ni, double sj, int64_t nj);
 
 inline int64_t cov_gen(int64_t sij, int64_t nij,
-  int64_t si, int64_t ni, int64_t sj, int64_t nj)
-{
-  return cov_i64_gen(sij, nij, si, ni, sj, nj);
-}
+  int64_t si, int64_t ni, int64_t sj, int64_t nj) {
+  return cov_i64_gen(sij, nij, si, ni, sj, nj); }
 
 inline int64_t cov_gen(int64_t sij, int32_t nij,
-  int64_t si, int32_t ni, int64_t sj, int32_t nj)
-{
-  return cov_i64_gen(sij, nij, si, ni, sj, nj);
-}
+  int64_t si, int32_t ni, int64_t sj, int32_t nj) {
+  return cov_i64_gen(sij, nij, si, ni, sj, nj); }
 
 inline int64_t cov_gen(int32_t sij, int32_t nij,
-  int32_t si, int32_t ni, int32_t sj, int32_t nj)
-{
-  return cov_i64_gen(sij, nij, si, ni, sj, nj);
-}
+  int32_t si, int32_t ni, int32_t sj, int32_t nj) {
+  return cov_i64_gen(sij, nij, si, ni, sj, nj); }
+
+inline int64_t cov_gen(int64_t sij, uint64_t nij,
+  int64_t si, uint64_t ni, int64_t sj, uint64_t nj) {
+  return cov_i64_gen(sij, nij, si, ni, sj, nj); }
+
+inline int64_t cov_gen(int64_t sij, uint32_t nij,
+  int64_t si, uint32_t ni, int64_t sj, uint32_t nj) {
+  return cov_i64_gen(sij, nij, si, ni, sj, nj); }
+
+inline int64_t cov_gen(int32_t sij, uint32_t nij,
+  int32_t si, uint32_t ni, int32_t sj, uint32_t nj) {
+  return cov_i64_gen(sij, nij, si, ni, sj, nj); }
 
 inline double cov_gen(double sij, int64_t nij,
-  double si, int64_t ni, double sj, int64_t nj)
-{
-  return cov_double_gen(sij, nij, si, ni, sj, nj);
-}
+  double si, int64_t ni, double sj, int64_t nj) {
+  return cov_double_gen(sij, nij, si, ni, sj, nj); }
 
 inline double cov_gen(double sij, int32_t nij,
-  double si, int32_t ni, double sj, int32_t nj)
-{
-  return cov_double_gen(sij, nij, si, ni, sj, nj);
-}
+  double si, int32_t ni, double sj, int32_t nj) {
+  return cov_double_gen(sij, nij, si, ni, sj, nj); }
+
+inline double cov_gen(double sij, uint64_t nij,
+  double si, uint64_t ni, double sj, uint64_t nj) {
+  return cov_double_gen(sij, nij, si, ni, sj, nj); }
+
+inline double cov_gen(double sij, uint32_t nij,
+  double si, uint32_t ni, double sj, uint32_t nj) {
+  return cov_double_gen(sij, nij, si, ni, sj, nj); }
 
 } } } // namespace calin::math::covariance_calc
