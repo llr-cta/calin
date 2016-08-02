3660d5e39b6978655e50495d77caf77cdc40f18f
Stephen Fegan <sfegan@gmail.com>
2016-08-02 22:04:20 +0100

    temporarily don't read header

--- src/iact_data/zfits_actl_data_source.cpp
+++ src/iact_data/zfits_actl_data_source.cpp
@@ -59,7 +59,7 @@ ZFITSSingleFileACTLDataSource(const std::string& filename,
     {
       std::unique_ptr<ACTL::IO::ProtobufIFits> rh_zfits {
         new ACTL::IO::ProtobufIFits(filename_.c_str(), "RunHeader") };
-      run_header_ = zfits_->readTypedMessage<DataModel::CameraRunHeader>(1);
+      //run_header_ = zfits_->readTypedMessage<DataModel::CameraRunHeader>(1);
     }
     catch(...)
     {
