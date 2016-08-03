82f3fbfdfbc4007172e6db963ee614fa718cfda2
Stephen Fegan <sfegan@gmail.com>
2016-08-03 10:28:03 +0100

    Test number of messages in RunHeader

--- src/iact_data/zfits_actl_data_source.cpp
+++ src/iact_data/zfits_actl_data_source.cpp
@@ -59,7 +59,8 @@ ZFITSSingleFileACTLDataSource(const std::string& filename,
     {
       std::unique_ptr<ACTL::IO::ProtobufIFits> rh_zfits {
         new ACTL::IO::ProtobufIFits(filename_.c_str(), "RunHeader") };
-      run_header_ = rh_zfits->readTypedMessage<DataModel::CameraRunHeader>(1);
+      if(rh_zfits->getNumMessagesInTable() > 0)
+        run_header_ = rh_zfits->readTypedMessage<DataModel::CameraRunHeader>(1);
       //LOG(INFO) << run_header_->DebugString();
     }
     catch(...)
