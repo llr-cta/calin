d5669ed1680276d0d20d9bf5d69ced4bed9a40ec
Stephen Fegan <sfegan@gmail.com>
2016-11-09 13:14:04 +0100

    Fix for displacement in IP sphere center

--- src/simulation/vso_raytracer.cpp
+++ src/simulation/vso_raytracer.cpp
@@ -621,7 +621,7 @@ bool VSORayTracer::testBeam(math::ray::Ray& photon,
     // Parallel beam - must set sampling radius
     Eigen::Vector3d
         beam_dir(-sin(theta)*sin(phi) ,-cos(theta), -sin(theta)*cos(phi));
-    Eigen::Vector3d beam_cen(0, 0, 0);
+    Eigen::Vector3d beam_cen = scope->reflectorIPCenter();
     math::ray::Ray ray(beam_cen, beam_dir);
     scope->reflectorToGlobal(ray);
     return laserBeam(photon, ray.position(), ray.direction(),
@@ -634,6 +634,7 @@ bool VSORayTracer::testBeam(math::ray::Ray& photon,
     Eigen::Vector3d
         beam_cen(Utantheta*sin(phi), U, Utantheta*cos(phi));
     double dist = beam_cen.norm();
+    beam_cen += scope->reflectorIPCenter();
     Eigen::Vector3d beam_dir(beam_cen*(-1.0/dist));
     math::ray::Ray ray(beam_cen, beam_dir);
     scope->reflectorToGlobal(ray);
