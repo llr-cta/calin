312d10203d26392f203fd6a70998a319facba1c0
Stephen Fegan <sfegan@gmail.com>
2017-02-10 13:25:51 +0100

    Force python to version >3

---
+++ configure_llrcta.sh
@@ -0,0 +1,5 @@
+cmake3 -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DFIND_LIBRARY_USE_LIB64_PATHS \
+  -DCMAKE_PREFIX_PATH=/opt/exp_soft/vo.cta.in2p3.fr/local \
+  -DCMAKE_INSTALL_PREFIX=$HOME/local \
+  ..
