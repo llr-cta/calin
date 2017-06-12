ddd51af1dea0059ac18acbf342e80162e59bc44b
Stephen Fegan <sfegan@gmail.com>
2017-06-12 06:24:42 +0200

    Add plausible LST configuration

--- python/simulation/vs_cta.py
+++ python/simulation/vs_cta.py
@@ -33,12 +33,14 @@ def dms(d,m,s):
         s = abs(s)
     return sign * (d + m/60.0 + s/3600.0)
 
-def mstn1_config(obscure_camera = True):
+def mstn1_config(obscure_camera = True, scope_x=0, scope_y=0):
     mst = calin.ix.simulation.vs_optics.IsotropicDCArrayParameters()
     mst.mutable_array_origin().set_latitude(dms(28, 45, 47.36))
     mst.mutable_array_origin().set_longitude(dms(-17, 53, 23.93))
     mst.mutable_array_origin().set_elevation(2147 * 100.0)
     scope = mst.mutable_prescribed_array_layout().add_scope_positions();
+    scope.set_x(scope_x)
+    scope.set_y(scope_y)
     scope.set_z(mst.array_origin().elevation())
     mst.mutable_reflector_frame().set_optic_axis_rotation(-90);
     dc = mst.mutable_reflector()
@@ -80,6 +82,57 @@ def mstn1_config(obscure_camera = True):
         obs_camera_box.aligned_box().set_incoming_only(True)
 
     return mst
+
+def lst1_config(obscure_camera = True, scope_x=0, scope_y=0):
+    lst = calin.ix.simulation.vs_optics.IsotropicDCArrayParameters()
+    lst.mutable_array_origin().set_latitude(dms(28, 45, 47.36))
+    lst.mutable_array_origin().set_longitude(dms(-17, 53, 23.93))
+    lst.mutable_array_origin().set_elevation(2147 * 100.0)
+    scope = lst.mutable_prescribed_array_layout().add_scope_positions();
+    scope.set_x(scope_x)
+    scope.set_y(scope_y)
+    scope.set_z(lst.array_origin().elevation())
+    lst.mutable_reflector_frame().set_optic_axis_rotation(0);
+    lst.mutable_reflector_frame().set_facet_grid_shift_z(44.46)
+    dc = lst.mutable_reflector()
+    dc.set_curvature_radius(5644) # Least square fit of sphere to mirror pos. Could be 5600 either
+    dc.mutable_psf_align().set_object_plane(numpy.inf) # 10 * 1e5);
+    dc.set_alignment_image_plane(2800)
+    dc.set_aperture(2*1153.3)
+    #dc.set_facet_num_hex_rings(8)
+    dc.set_facet_spacing(154)
+    dc.set_facet_size(151)
+    dc.set_facet_focal_length(0)
+    dc.set_facet_focal_length_dispersion(10)
+    dc.set_facet_spot_size_probability(0.8)
+    dc.set_facet_spot_size(0.5 * 1.4) # Spot size of 14mm at 2F
+    dc.set_facet_spot_size_dispersion(0.5 * 0.02)
+    dc.set_facet_labeling_parity(True)
+    dc.set_weathering_factor(1.0)
+    for id in [0,127,148,171,191]: dc.add_facet_missing_list(id)
+    lst.mutable_focal_plane().set_camera_diameter(235)
+    lst.mutable_focal_plane().mutable_translation().set_y(1/(1.0/dc.alignment_image_plane()-1/(10 * 1e5)))
+    lst.mutable_pixel().set_spacing(5)
+    lst.mutable_pixel().set_cone_inner_diameter(5)
+    lst.mutable_pixel().set_cone_survival_prob(1)
+    lst.mutable_pixel().set_hex_module_size(1)
+    lst.mutable_pixel().set_module_num_hex_rings(9)
+    u1,v1 = calin.math.hex_array.cluster_hexid_to_center_uv(1,1)
+    x1,y1 = calin.math.hex_array.uv_to_xy(u1,v1)
+    rot = numpy.arctan2(-y1,x1)/numpy.pi*180 - 30
+    lst.mutable_pixel().set_grid_rotation(rot)
+
+    if(obscure_camera):
+        obs_camera_box = lst.add_obscurations()
+        obs_camera_box.aligned_box().max_corner().set_x(150)
+        obs_camera_box.aligned_box().max_corner().set_y(lst.focal_plane().translation().y()+150)
+        obs_camera_box.aligned_box().max_corner().set_z(150)
+        obs_camera_box.aligned_box().min_corner().set_x(-150)
+        obs_camera_box.aligned_box().min_corner().set_y(lst.focal_plane().translation().y())
+        obs_camera_box.aligned_box().min_corner().set_z(-150)
+        obs_camera_box.aligned_box().set_incoming_only(True)
+
+    return lst
 
 def make_array(cfg, rng = calin.math.rng.RNG()):
     array = calin.simulation.vs_optics.VSOArray()
