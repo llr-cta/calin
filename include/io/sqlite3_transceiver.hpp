933035c6acc8e4fb3a9aa61d17b4e85794875416
Stephen Fegan <sfegan@gmail.com>
2017-10-25 13:56:18 +0200

    Fix long latent bug in SQLLite3 transceiver stopping file from being closed

--- include/io/sqlite3_transceiver.hpp
+++ include/io/sqlite3_transceiver.hpp
@@ -1,4 +1,4 @@
-/* 
+/*
 
    calin/io/sqlite3_transceiver.hpp -- Stephen Fegan -- 2015-09-08
 
@@ -8,11 +8,11 @@
    LLR, Ecole polytechnique, CNRS/IN2P3, Universite Paris-Saclay
 
    This file is part of "calin"
-   
+
    "calin" is free software: you can redistribute it and/or modify it
    under the terms of the GNU General Public License version 2 or
    later, as published by the Free Software Foundation.
-    
+
    "calin" is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
@@ -39,20 +39,19 @@ namespace calin { namespace io { namespace sql_transceiver {
 
 class SQLite3Transceiver: public SQLTransceiver
 {
- public:
+public:
   enum OpenMode { EXISTING_OR_NEW_RW, EXISTING_RW, TRUNCATE_RW, READ_ONLY };
-  
+
   SQLite3Transceiver(const std::string& filename,
                      OpenMode open_mode = EXISTING_OR_NEW_RW,
                      bool write_sql_to_log = false);
   virtual ~SQLite3Transceiver();
 
- protected:
-
-  sqlite3* db_;
-  bool inherit_db_ = false;
+protected:
+  sqlite3* db_ = nullptr;
+  bool adopt_db_ = false;
   OpenMode open_mode_ = EXISTING_OR_NEW_RW;
-  
+
   SQLStatement* prepare_statement(const std::string& sql) override;
 
   bool begin_transaction() override;
