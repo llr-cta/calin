c038ac336fb0eaa0c8df92f1f2bb6dd40504c48a
Stephen Fegan <sfegan@gmail.com>
2019-02-21 10:55:56 +0100

    Make special::SQR a template

--- include/math/special.hpp
+++ include/math/special.hpp
@@ -1,19 +1,19 @@
-/* 
+/*
 
    calin/math/special.hpp -- Stephen Fegan -- 2015-08-06
 
-   Various special functions, which could be implemented as interfaces 
+   Various special functions, which could be implemented as interfaces
    to GSL or directly
 
    Copyright 2015, Stephen Fegan <sfegan@llr.in2p3.fr>
    LLR, Ecole Polytechnique, CNRS/IN2P3
 
    This file is part of "calin"
-   
+
    "calin" is free software: you can redistribute it and/or modify it
    under the terms of the GNU General Public License version 2 or
    later, as published by the Free Software Foundation.
-    
+
    "calin" is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
@@ -28,9 +28,9 @@
 
 namespace calin { namespace math { namespace special {
 
-inline double SQR(double x) { return x*x; }
-inline double CUBE(double x) { return x*x*x; }
-inline double QUAD(double x) { return SQR(SQR(x)); }
+template<typename T> inline T SQR(T x) { return x*x; }
+template<typename T> inline T CUBE(T x) { return x*x*x; }
+template<typename T> inline T QUAD(T x) { return SQR(SQR(x)); }
 
 inline double dawson(double x)
 {
@@ -41,5 +41,5 @@ inline double lerfc(double x)
 {
   return gsl_sf_log_erfc(x);
 }
-  
+
 } } } // namespace calin::math::special
