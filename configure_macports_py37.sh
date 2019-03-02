6ba160750ecca4e52941cb2b5bd2a27aea9709ed
Stephen Fegan <sfegan@gmail.com>
2019-03-02 08:26:04 +0000

    Force cmake to use g++ and gcc
    Seems necessary after upgrade to Mojave... but why?

--- configure_macports_py37.sh
+++ configure_macports_py37.sh
@@ -1,4 +1,5 @@
 cmake \ #--graphviz=calin_deps.dot \
+    -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ \
     -DPYTHON_EXECUTABLE=/opt/local/bin/python3.7 \
     -DPYTHON_INCLUDE_DIR=/opt/local/Library/Frameworks/Python.framework/Versions/3.7/include/python3.7m \
     -DPYTHON_LIBRARY=/opt/local/Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/config-3.7m-darwin/libpython3.7.dylib \
