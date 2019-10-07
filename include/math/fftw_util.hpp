55e60ef95afd80f989f5c952a9a6014412d59f0d
Stephen Fegan <sfegan@gmail.com>
2019-10-07 13:08:19 +0200

    Add hcvec functions for polynomial expansion

--- include/math/fftw_util.hpp
+++ include/math/fftw_util.hpp
@@ -22,6 +22,7 @@
 
 #pragma once
 
+#include<util/log.hpp>
 #include<math/fftw_util.pb.h>
 
 namespace calin { namespace math { namespace fftw_util {
@@ -99,6 +100,16 @@ void hcvec_scale_and_multiply_conj(T* ovec, const T* ivec1,
 }
 
 template<typename T>
+void hcvec_scale(T* ovec, unsigned nsample,
+  T scale = 1.0)
+{
+  T *ro = ovec;
+  T *re = ovec + nsample;
+  while(ro<re)
+    *(ro++) *= scale;
+}
+
+template<typename T>
 void hcvec_scale_and_add(T* ovec, const T* ivec, unsigned nsample,
   T scale = 1.0)
 {
@@ -107,6 +118,81 @@ void hcvec_scale_and_add(T* ovec, const T* ivec, unsigned nsample,
   const T *ri = ivec;
   while(ro<re)
     *(ro++) += *(ri++)*scale;
+}
+
+template<typename T>
+void hcvec_set_real(T* ovec, T real_value, unsigned nsample)
+{
+  T *ro = ovec;
+  T *co = ovec + nsample-1;
+  (*ro++) = real_value;
+  while(ro < co)
+  {
+    (*ro++) = real_value;
+    (*co--) = T(0);
+  }
+  if(ro==co)(*ro) = real_value;
+}
+
+template<typename T>
+void hcvec_multiply_and_add_real(T* ovec, const T* ivec1,
+  const T* ivec2, T real_addand, unsigned nsample, T scale = 1.0)
+{
+  T *ro = ovec;
+  T *co = ovec + nsample-1;
+  const T *ri1 = ivec1;
+  const T *ci1 = ivec1 + nsample-1;
+  const T *ri2 = ivec2;
+  const T *ci2 = ivec2 + nsample-1;
+
+  (*ro++) = (*ri1++) * (*ri2++) * scale + real_addand;
+  if(ro==ri1 or ro==ri2)
+  {
+    while(ro < co)
+    {
+      T vri1 = *ri1++;
+      T vci1 = *ci1--;
+      T vri2 = *ri2++;
+      T vci2 = *ci2--;
+      (*ro++) = (vri1*vri2 - vci1*vci2) * scale + real_addand;
+      (*co--) = (vri1*vci2 + vci1*vri2) * scale;
+    }
+   }
+  else
+  {
+    while(ro < co)
+    {
+      (*ro++) = ((*ri1)*(*ri2) - (*ci1)*(*ci2)) * scale + real_addand;
+      (*co--) = ((*ri1++)*(*ci2--) + (*ci1--)*(*ri2++)) * scale;
+    }
+  }
+  if(ro==co)(*ro) = (*ri1) * (*ri2) * scale + real_addand;
+}
+
+template<typename T>
+void hcvec_polynomial(T* ovec, const T* ivec, const std::vector<T>& p, unsigned nsample, T scale = 1.0)
+{
+  if(p.empty()) {
+    hcvec_set_real(ovec, T(0), nsample);
+    return;
+  }
+
+  auto pi = p.end();
+
+  --pi;
+  hcvec_set_real(ovec, *pi, nsample);
+  // calin::util::log::LOG(calin::util::log::INFO) << *pi;
+
+  // for(unsigned i=0; i<nsample; i++)
+  //   calin::util::log::LOG(calin::util::log::INFO) << ovec[i] << ' ' << ivec[i];
+
+  while(pi != p.begin()) {
+    --pi;
+    // calin::util::log::LOG(calin::util::log::INFO) << *pi;
+    hcvec_multiply_and_add_real(ovec, ovec, ivec, *pi, nsample, scale);
+    // for(unsigned i=0; i<nsample; i++)
+    //   calin::util::log::LOG(calin::util::log::INFO) << ovec[i] << ' ' << ivec[i];
+  }
 }
 
 #endif
