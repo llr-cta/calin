f6fe512f26fd4324c1becd0ae1680487535b653d
Stephen Fegan <sfegan@gmail.com>
2020-03-04 08:44:57 +0100

    Try adding unroll pragma for GCC

--- include/math/lomb_scargle_vcl.hpp
+++ include/math/lomb_scargle_vcl.hpp
@@ -149,6 +149,7 @@ typename VCLReal::vecX_t periodogram_vcl(
       c.load_a(vcf + iblock*VCLReal::num_real);
       s.load_a(vsf + iblock*VCLReal::num_real);
 
+#pragma GCC unroll UNROLL
 #pragma clang loop unroll(full)
       for(unsigned iroll=0; iroll<UNROLL; ++iroll) {
         CC[iroll] = vcl::mul_add(c,c,CC[iroll]);
@@ -319,6 +320,7 @@ typename VCLReal::matX_t multi_periodogram_vcl(
       c.load_a(vxcf + ix);
       s.load_a(vxsf + ix);
 
+#pragma GCC unroll UNROLL
 #pragma clang loop unroll(full)
       for(unsigned iroll=0;iroll<UNROLL;++iroll) {
         CC[iroll] = vcl::mul_add(c,c,CC[iroll]);
@@ -427,6 +429,7 @@ typename VCLReal::matX_t multi_periodogram_vcl(
         xc.load_a(vxcf + ix);
         xs.load_a(vxsf + ix);
 
+#pragma GCC unroll UNROLL
 #pragma clang loop unroll(full)
         for(unsigned iroll=0;iroll<UNROLL;++iroll) {
           XC[iroll] += xc;
