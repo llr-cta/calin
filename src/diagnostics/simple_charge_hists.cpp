5b50860c22d225638eb27dadd37674caec4dafb6
Stephen Fegan <sfegan@gmail.com>
2020-05-09 16:07:26 +0200

    First implementation of simple charge hists

--- src/diagnostics/simple_charge_hists.cpp
+++ src/diagnostics/simple_charge_hists.cpp
@@ -48,7 +48,7 @@ SimpleChargeHistsParallelEventVisitor(
 
 SimpleChargeHistsParallelEventVisitor::~SimpleChargeHistsParallelEventVisitor()
 {
-  // for(auto* h : chan_hists_)delete h;
+  for(auto* h : chan_hists_)delete h;
 }
 
 SimpleChargeHistsParallelEventVisitor* SimpleChargeHistsParallelEventVisitor::new_sub_visitor(
@@ -68,47 +68,15 @@ bool SimpleChargeHistsParallelEventVisitor::visit_telescope_run(
   const calin::ix::iact_data::telescope_run_configuration::TelescopeRunConfiguration* run_config,
   calin::iact_data::event_visitor::EventLifetimeManager* event_lifetime_manager)
 {
-  // partials_.Clear();
-  // results_.Clear();
-  //
-  // has_dual_gain_ = (run_config->camera_layout().adc_gains() !=
-  //   calin::ix::iact_data::instrument_layout::CameraLayout::SINGLE_GAIN);
-  // for(unsigned ichan=0;ichan<run_config->configured_channel_id_size();++ichan) {
-  //   partials_.add_channel();
-  // }
-  //
-  // for(auto* h :chan_hists_)delete h;
-  // chan_hists_.resize(run_config->configured_channel_id_size());
-  // for(auto*& h : chan_hists_) {
-  //   h = new ChannelHists(has_dual_gain_, config_.ped_time_hist_resolution(),
-  //     config_.dark_hist_hg_resolution(), config_.bright_hist_hg_resolution(),
-  //     config_.dark_hist_lg_resolution(), config_.bright_hist_lg_resolution());
-  // }
+  has_dual_gain_ = (run_config->camera_layout().adc_gains() !=
+    calin::ix::iact_data::instrument_layout::CameraLayout::SINGLE_GAIN);
 
-  // if(high_gain_visitor_) {
-  //   high_gain_results_->Clear();
-  //   high_gain_results_->set_integration_n(high_gain_visitor_->window_n());
-  //   high_gain_results_->set_bkg_integration_0(high_gain_visitor_->bkg_window_0());
-  //   int sig_win_0 = -2;
-  //   for(int sw0 : high_gain_visitor_->sig_window_0()) {
-  //     if(sig_win_0==-2)sig_win_0 = sw0;
-  //     else if(sig_win_0 != sw0)sig_win_0 = -1;
-  //     high_gain_results_->add_chan_sig_integration_0(sw0);
-  //   };
-  //   high_gain_results_->sig_integration_0(std::max(sig_win_0,-1));
-  // }
-  // if(low_gain_visitor_) {
-  //   low_gain_results_->Clear();
-  //   low_gain_results_->set_integration_n(low_gain_visitor_->window_n());
-  //   low_gain_results_->set_bkg_integration_0(low_gain_visitor_->bkg_window_0());
-  //   int sig_win_0 = -2;
-  //   for(int sw0 : low_gain_visitor_->sig_window_0()) {
-  //     if(sig_win_0==-2)sig_win_0 = sw0;
-  //     else if(sig_win_0 != sw0)sig_win_0 = -1;
-  //     low_gain_results_->add_chan_sig_integration_0(sw0);
-  //   };
-  //   low_gain_results_->sig_integration_0(std::max(sig_win_0,-1));
-  // }
+  for(auto* h :chan_hists_)delete h;
+  chan_hists_.resize(run_config->configured_channel_id_size());
+  for(auto*& h : chan_hists_) {
+    h = new ChannelHists(has_dual_gain_, config_);
+  }
+
   return true;
 }
 
@@ -117,103 +85,124 @@ bool SimpleChargeHistsParallelEventVisitor::leave_telescope_run()
   return true;
 }
 
-#if 0
-void SimpleChargeHistsParallelEventVisitor::record_one_gain_channel_data(
-  const calin::ix::iact_data::telescope_event::TelescopeEvent* event,
-  const calin::iact_data::waveform_treatment_event_visitor::OptimalWindowSumWaveformTreatmentParallelEventVisitor* sum_visitor,
-  unsigned ichan, double elapsed_event_time,
-  calin::ix::diagnostics::simple_charge_stats::PartialOneGainChannelSimpleChargeStats* one_gain_stats,
-  SingleGainChannelHists* one_gain_hists)
-{
-  one_gain_stats->increment_all_trig_num_events();
-  one_gain_stats->increment_all_trig_ped_win_sum(sum_visitor->array_chan_bkg_win_sum()[ichan]);
-  one_gain_stats->increment_all_trig_ped_win_sumsq(SQR(sum_visitor->array_chan_bkg_win_sum()[ichan]));
-  if(event->trigger_type() == calin::ix::iact_data::telescope_event::TRIGGER_PEDESTAL) {
-    double wf_all_sum = sum_visitor->array_chan_all_sum()[ichan];
-    double sqr_wf_all_sum = SQR(wf_all_sum);
-    one_gain_stats->increment_ped_trig_num_events();
-    one_gain_stats->increment_ped_trig_full_wf_sum(wf_all_sum);
-    one_gain_stats->increment_ped_trig_full_wf_sumsq(sqr_wf_all_sum);
-    one_gain_hists->ped_wf_q_sum->insert(wf_all_sum);
-    one_gain_hists->ped_wf_1_sum_vs_time->insert(elapsed_event_time);
-    one_gain_hists->ped_wf_q_sum_vs_time->insert(elapsed_event_time, wf_all_sum);
-    one_gain_hists->ped_wf_q2_sum_vs_time->insert(elapsed_event_time, sqr_wf_all_sum);
-  } else if(event->trigger_type() == calin::ix::iact_data::telescope_event::TRIGGER_EXTERNAL_FLASHER) {
-    one_gain_hists->ext_opt_win_q_sum->insert(sum_visitor->array_chan_opt_win_sum()[ichan]);
-    one_gain_hists->ext_opt_win_index->insert(sum_visitor->array_chan_opt_win_index()[ichan]);
-    one_gain_hists->ext_opt_win_max->insert(sum_visitor->array_chan_max()[ichan]);
-  } else if(event->trigger_type() == calin::ix::iact_data::telescope_event::TRIGGER_PHYSICS) {
-    one_gain_hists->phys_opt_win_q_sum->insert(sum_visitor->array_chan_opt_win_sum()[ichan]);
-    one_gain_hists->phys_opt_win_index->insert(sum_visitor->array_chan_opt_win_index()[ichan]);
-  }
-}
-
 void SimpleChargeHistsParallelEventVisitor::record_one_visitor_data(
   uint64_t seq_index, const calin::ix::iact_data::telescope_event::TelescopeEvent* event,
-  const calin::iact_data::waveform_treatment_event_visitor::OptimalWindowSumWaveformTreatmentParallelEventVisitor* sum_visitor,
-  calin::ix::diagnostics::simple_charge_stats::PartialSimpleChargeStats* partials)
+  const calin::iact_data::waveform_treatment_event_visitor::OptimalWindowSumWaveformTreatmentParallelEventVisitor* sum_visitor)
 {
-  double elapsed_event_time = event->elapsed_event_time().time_ns() * 1e-9;
+  if(not sum_visitor or not sum_visitor->is_same_event(seq_index))
+    return;
 
-  if(sum_visitor and sum_visitor->is_same_event(seq_index)) {
-    for(unsigned ichan=0; ichan<sum_visitor->nchan(); ichan++) {
-      auto* pc = partials->mutable_channel(ichan);
-      switch(sum_visitor->array_chan_signal_type()[ichan]) {
-      case calin::ix::iact_data::telescope_event::SIGNAL_UNIQUE_GAIN:
-      case calin::ix::iact_data::telescope_event::SIGNAL_HIGH_GAIN:
-        record_one_gain_channel_data(event, sum_visitor, ichan, elapsed_event_time,
-          pc->mutable_high_gain(), chan_hists_[ichan]->high_gain);
-        break;
-      case calin::ix::iact_data::telescope_event::SIGNAL_LOW_GAIN:
-        record_one_gain_channel_data(event, sum_visitor, ichan, elapsed_event_time,
-          pc->mutable_low_gain(), chan_hists_[ichan]->low_gain);
-        break;
-      case calin::ix::iact_data::telescope_event::SIGNAL_NONE:
-      default:
-        // do nothing
-        break;
-      }
+  for(unsigned ichan=0; ichan<sum_visitor->nchan(); ichan++) {
+    SingleGainChannelHists* hists = nullptr;
+    switch(sum_visitor->array_chan_signal_type()[ichan]) {
+    case calin::ix::iact_data::telescope_event::SIGNAL_UNIQUE_GAIN:
+    case calin::ix::iact_data::telescope_event::SIGNAL_HIGH_GAIN:
+      hists = chan_hists_[ichan]->high_gain;
+      break;
+    case calin::ix::iact_data::telescope_event::SIGNAL_LOW_GAIN:
+      hists = chan_hists_[ichan]->low_gain;
+      break;
+    case calin::ix::iact_data::telescope_event::SIGNAL_NONE:
+    default:
+      // do nothing
+      break;
+    }
+
+    if(hists) {
+      if(hists->full_wf_qsum)
+        hists->full_wf_qsum->insert(sum_visitor->array_chan_all_sum()[ichan]);
+      if(hists->full_wf_max)
+        hists->full_wf_max->insert(sum_visitor->array_chan_max()[ichan]);
+      if(hists->full_wf_max_index)
+        hists->full_wf_max_index->insert(sum_visitor->array_chan_max_index()[ichan]);
+      if(hists->opt_win_qsum)
+        hists->opt_win_qsum->insert(sum_visitor->array_chan_opt_win_sum()[ichan]);
+      if(hists->opt_win_qtsum)
+        hists->opt_win_qtsum->insert(sum_visitor->array_chan_opt_win_sum_qt()[ichan]);
+      if(hists->opt_win_index)
+        hists->opt_win_index->insert(sum_visitor->array_chan_opt_win_index()[ichan]);
+      if(hists->ped_win_qsum)
+        hists->ped_win_qsum->insert(sum_visitor->array_chan_bkg_win_sum()[ichan]);
+      if(hists->sig_win_qsum)
+        hists->sig_win_qsum->insert(sum_visitor->array_chan_sig_win_sum()[ichan]);
     }
   }
 }
-#endif
 
 bool SimpleChargeHistsParallelEventVisitor::visit_telescope_event(uint64_t seq_index,
   calin::ix::iact_data::telescope_event::TelescopeEvent* event)
 {
   if(high_gain_visitor_) {
-    // record_one_visitor_data(seq_index, event, high_gain_visitor_, &partials_);
+    record_one_visitor_data(seq_index, event, high_gain_visitor_);
   }
   if(low_gain_visitor_) {
-    // record_one_visitor_data(seq_index, event, low_gain_visitor_, &partials_);
+    record_one_visitor_data(seq_index, event, low_gain_visitor_);
   }
   return true;
+}
+
+void SimpleChargeHistsParallelEventVisitor::merge_one_gain_hists(SingleGainChannelHists* into,
+  const SingleGainChannelHists* from)
+{
+  if(from->full_wf_qsum)into->full_wf_qsum->insert_hist(*from->full_wf_qsum);
+  if(from->full_wf_max)into->full_wf_max->insert_hist(*from->full_wf_max);
+  if(from->full_wf_max_index)into->full_wf_max_index->insert_hist(*from->full_wf_max_index);
+  if(from->opt_win_qsum)into->opt_win_qsum->insert_hist(*from->opt_win_qsum);
+  if(from->opt_win_qtsum)into->opt_win_qtsum->insert_hist(*from->opt_win_qtsum);
+  if(from->opt_win_index)into->opt_win_index->insert_hist(*from->opt_win_index);
+  if(from->ped_win_qsum)into->ped_win_qsum->insert_hist(*from->ped_win_qsum);
+  if(from->sig_win_qsum)into->sig_win_qsum->insert_hist(*from->sig_win_qsum);
 }
 
 bool SimpleChargeHistsParallelEventVisitor::merge_results()
 {
   if(parent_) {
-    // parent_->partials_.IntegrateFrom(partials_);
+    for(unsigned ichan=0; ichan<chan_hists_.size(); ichan++) {
+      if(chan_hists_[ichan]->high_gain) {
+        merge_one_gain_hists(parent_->chan_hists_[ichan]->high_gain, chan_hists_[ichan]->high_gain);
+      }
+      if(chan_hists_[ichan]->low_gain) {
+        merge_one_gain_hists(parent_->chan_hists_[ichan]->low_gain, chan_hists_[ichan]->low_gain);
+      }
+    }
   }
   return true;
 }
 
-#if 0
-calin::ix::diagnostics::simple_charge_stats::SimpleChargeStats*
-SimpleChargeStatsParallelEventVisitor::simple_charge_stats(
-  calin::ix::diagnostics::simple_charge_stats::SimpleChargeStats* stats) const
+void SimpleChargeHistsParallelEventVisitor::extract_one_gain_hists(
+  calin::ix::diagnostics::simple_charge_hists::OneGainSimpleChargeHists* into,
+  const SingleGainChannelHists* from) const
 {
-  if(stats == nullptr)stats = results_.New();
-  stats->CopyFrom(results_);
-  return stats;
+  if(from->full_wf_qsum)from->full_wf_qsum->serialize(into->mutable_full_wf_qsum());
+  if(from->full_wf_max)from->full_wf_max->serialize(into->mutable_full_wf_max());
+  if(from->full_wf_max_index)from->full_wf_max_index->serialize(into->mutable_full_wf_max_index());
+  if(from->opt_win_qsum)from->opt_win_qsum->serialize(into->mutable_opt_win_qsum());
+  if(from->opt_win_qtsum)from->opt_win_qtsum->serialize(into->mutable_opt_win_qtsum());
+  if(from->opt_win_index)from->opt_win_index->serialize(into->mutable_opt_win_index());
+  if(from->ped_win_qsum)from->ped_win_qsum->serialize(into->mutable_ped_win_qsum());
+  if(from->sig_win_qsum)from->sig_win_qsum->serialize(into->mutable_sig_win_qsum());
 }
-#endif
 
 calin::ix::diagnostics::simple_charge_hists::SimpleChargeHists*
 SimpleChargeHistsParallelEventVisitor::simple_charge_hists(
-  calin::ix::diagnostics::simple_charge_hists::SimpleChargeHists* stats) const
+  calin::ix::diagnostics::simple_charge_hists::SimpleChargeHists* hists) const
 {
-  return nullptr;
+  if(hists == nullptr) {
+    hists = new calin::ix::diagnostics::simple_charge_hists::SimpleChargeHists();
+  } else {
+    hists->Clear();
+  }
+
+  for(unsigned ichan=0; ichan<chan_hists_.size(); ichan++) {
+    if(chan_hists_[ichan]->high_gain) {
+      extract_one_gain_hists(hists->add_high_gain_channel(), chan_hists_[ichan]->high_gain);
+    }
+    if(chan_hists_[ichan]->low_gain) {
+      extract_one_gain_hists(hists->add_low_gain_channel(), chan_hists_[ichan]->low_gain);
+    }
+  }
+
+  return hists;
 }
 
 namespace {
@@ -317,6 +306,39 @@ namespace {
   }
 
 } // anonymous namespace
+
+calin::ix::diagnostics::simple_charge_hists::SimpleChargeHistsConfig
+SimpleChargeHistsParallelEventVisitor::all_enabled_config(
+  double qsum_dx, int qsum_nmax, int qsum_rebin,
+  double max_dx, int max_nmax, int max_rebin)
+{
+  calin::ix::diagnostics::simple_charge_hists::SimpleChargeHistsConfig config;
+  auto* hg_config = config.mutable_high_gain();
+  pre_fill_hist_config(hg_config, "pedestal", "high",
+    qsum_dx, qsum_nmax, qsum_rebin, max_dx, max_nmax, max_rebin);
+  hg_config->mutable_full_wf_qsum()->set_enable(true);
+  hg_config->mutable_full_wf_max()->set_enable(true);
+  hg_config->mutable_full_wf_max_index()->set_enable(true);
+  hg_config->mutable_opt_win_qsum()->set_enable(true);
+  hg_config->mutable_opt_win_qtsum ()->set_enable(true);
+  hg_config->mutable_opt_win_index()->set_enable(true);
+  hg_config->mutable_ped_win_qsum()->set_enable(true);
+  hg_config->mutable_sig_win_qsum()->set_enable(true);
+
+  auto* lg_config = config.mutable_low_gain();
+  pre_fill_hist_config(lg_config, "pedestal", "low",
+    qsum_dx, qsum_nmax, qsum_rebin, max_dx, max_nmax, max_rebin);
+  lg_config->mutable_full_wf_qsum()->set_enable(true);
+  lg_config->mutable_full_wf_max()->set_enable(true);
+  lg_config->mutable_full_wf_max_index()->set_enable(true);
+  lg_config->mutable_opt_win_qsum()->set_enable(true);
+  lg_config->mutable_opt_win_qtsum ()->set_enable(true);
+  lg_config->mutable_opt_win_index()->set_enable(true);
+  lg_config->mutable_ped_win_qsum()->set_enable(true);
+  lg_config->mutable_sig_win_qsum()->set_enable(true);
+
+  return config;
+}
 
 calin::ix::diagnostics::simple_charge_hists::SimpleChargeHistsConfig
 SimpleChargeHistsParallelEventVisitor::ped_trig_default_config()
