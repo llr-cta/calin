b6f7d92d817603d9a7c59103df5e96405583ac76
Stephen Fegan <sfegan@gmail.com>
2020-05-27 07:16:36 +0200

    Add scaled accumulate

--- include/math/accumulator.hpp
+++ include/math/accumulator.hpp
@@ -40,6 +40,7 @@ public:
   void reset(double val=0) { sum_ = val; }
   void accumulate(const value_type x) { sum_ += x; }
   void accumulate_from(const SimpleAccumulator& o) { sum_ += o.sum_; }
+  void accumulate_from_with_scaling(const SimpleAccumulator& o, double s) { sum_ += s*o.sum_; }
   value_type total() const { return sum_; };
   value_type correction() const { return 0; }
   operator value_type() const { return total(); }
@@ -65,7 +66,13 @@ public:
     sum_=T;
   }
   void accumulate_from(const SimpleAccumulator& o) { accumulate(o.total()); }
-  void accumulate_from(const BasicKahanAccumulator<value_type>& o) { accumulate(o.sum_); accumulate(o.cor_); }
+  void accumulate_from(const BasicKahanAccumulator<value_type>& o) {
+    accumulate(o.total()); accumulate(o.correction()); }
+
+  void accumulate_from_with_scaling(const SimpleAccumulator& o, double s) { accumulate(s*o.total()); }
+  void accumulate_from_with_scaling(const BasicKahanAccumulator<value_type>& o, double s) {
+    accumulate(s*o.total()); accumulate(s*o.correction()); }
+
   value_type total() const { return sum_; }
   value_type correction() const { return -cor_; }
   std::vector<value_type> corrections() const { return { correction() }; }
