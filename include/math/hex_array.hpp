8c0705e991b94235571099dea0a3ac4b07530ac3
Stephen Fegan <sfegan@gmail.com>
2020-11-05 11:30:13 +0100

    Use double precision hexid->ringid algorithm by default

--- include/math/hex_array.hpp
+++ include/math/hex_array.hpp
@@ -55,6 +55,19 @@ void nh_to_xy(int *, double *, double *);
 
 inline unsigned positive_hexid_to_ringid_root(unsigned hexid)
 {
+  // Adapted from VCL version
+  const double four_over_three = 4.0/3.0;
+  const double one_over_three = 1.0/3.0;
+  double arg = hexid;
+  arg = arg * four_over_three + one_over_three;
+  arg = std::sqrt(arg);
+  const double one_half = 0.5;
+  arg = arg * one_half + one_half;
+  return arg;
+}
+
+inline unsigned positive_hexid_to_ringid_float_root(unsigned hexid)
+{
   // See: http://www.codecodex.com/wiki/Calculate_an_integer_square_root
   const unsigned iarg = 1+4*(hexid-1)/3;
   // return static_cast<unsigned>(floor((std::sqrt(iarg)-1)/2)+1);
