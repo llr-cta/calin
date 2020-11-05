89eb05215f548535d3a270045965b7c2d5e9f9da
Stephen Fegan <sfegan@gmail.com>
2020-11-05 11:49:42 +0100

    Switch between float and double according to hexid

--- include/math/hex_array.hpp
+++ include/math/hex_array.hpp
@@ -53,7 +53,7 @@ void nh_to_xy(int *, double *, double *);
 //
 // *****************************************************************************
 
-inline unsigned positive_hexid_to_ringid_root(unsigned hexid)
+inline unsigned positive_hexid_to_ringid_double_root(unsigned hexid)
 {
   // Adapted from VCL version
   const double four_over_three = 4.0/3.0;
@@ -76,6 +76,15 @@ inline unsigned positive_hexid_to_ringid_float_root(unsigned hexid)
   // sqrt(float) is quicker here but loses precision by iarg ~= 2^24 -
   // you have been warned :-)
   return (unsigned(std::sqrt(float(iarg)))+1)/2;
+}
+
+inline unsigned positive_hexid_to_ringid_root(unsigned hexid)
+{
+  if(hexid < 12589056) {
+    return positive_hexid_to_ringid_float_root(hexid);
+  } else {
+    return positive_hexid_to_ringid_double_root(hexid);
+  }
 }
 
 inline unsigned positive_hexid_to_ringid_loop(unsigned hexid)
