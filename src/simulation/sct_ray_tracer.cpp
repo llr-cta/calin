9aef7804a4e276083c941ab7e533af6eec49dc26
Stephen Fegan <sfegan@gmail.com>
2021-05-03 12:01:07 +0200

    Add random facet offsets & direction

--- src/simulation/sct_ray_tracer.cpp
+++ src/simulation/sct_ray_tracer.cpp
@@ -27,6 +27,7 @@
 #include <util/log.hpp>
 #include <math/geometry.hpp>
 #include <math/vector3d_util.hpp>
+#include <simulation/sct_facet_scheme.hpp>
 #include <simulation/sct_ray_tracer.hpp>
 
 using namespace calin::simulation::sct_optics;
@@ -92,6 +93,52 @@ calin::simulation::sct_optics::make_sct_telescope(
     calin::math::geometry::quaternion_to_euler(
       telescope->mutable_primary_rotation(), primary_rotation);
   }
+  telescope->mutable_primary_facet_scheme()->CopyFrom(param.primary_facet_scheme());
+
+  auto primary_facet_scheme = calin::simulation::sct_optics::SCTPrimaryFacetScheme(
+    param.primary_facet_scheme());
+  for(unsigned ifacet=0; ifacet<primary_facet_scheme.num_facets(); ++ifacet) {
+    auto* facet = telescope->add_primary_facets();
+    facet->set_id(ifacet);
+    Eigen::Vector3d facet_offset(0,0,0);
+    if(param.primary_facet_offset_xz_dispersion() > 0) {
+      facet_offset.x() += param.primary_facet_offset_xz_dispersion()*rng->normal();
+      facet_offset.z() += param.primary_facet_offset_xz_dispersion()*rng->normal();
+      calin::math::vector3d_util::dump_as_proto(facet_offset, facet->mutable_offset());
+    }
+    if(param.primary_facet_offset_y_dispersion() > 0) {
+      facet_offset.y() += param.primary_facet_offset_y_dispersion()*rng->normal();
+      calin::math::vector3d_util::dump_as_proto(facet_offset, facet->mutable_offset());
+    }
+    if(param.primary_facet_rotation_dispersion() > 0) {
+      auto* facet_euler = facet->mutable_rotation();
+      facet_euler->set_rotation_order(calin::ix::common_types::EulerAngles3D::XYX);
+      calin::math::geometry::scattering_euler(facet_euler,
+        param.primary_facet_rotation_dispersion(), *rng);
+    }
+  }
+  for(auto id : param.primary_facets_removed()) {
+    if(id < primary_facet_scheme.num_facets()) {
+      telescope->mutable_primary_facets(id)->set_removed(true);
+    }
+  }
+
+/*
+  message SCTFacet
+  {
+    uint32 id                                                = 1
+      [(CFO).desc = "Sequential facet identification number, starting at zero."];
+    bool removed                                             = 2
+      [(CFO).desc = "True if this mirror facet has been removed."];
+    double spot_size                                         = 3 [
+      (CFO).desc = "Parameter describing spot size from this mirror in "
+        "perfectly aligned system.", (CFO).units="cm" ];
+    calin.ix.common_types.Vector3D offset                    = 4 [
+      (CFO).desc = "Facet offset, if needed.", (CFO).units="cm" ];
+    calin.ix.common_types.EulerAngles3D rotation             = 5 [
+      (CFO).desc = "Facet rotation angles, if needed." ];
+  };
+*/
 
   // calin.ix.common_types.Matrix3D primary_rotation          = 12 [
   //   (CFO).desc = "Primary rotation matrix, if needed." ];
