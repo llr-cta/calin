83b0266a3d2521705800aea090db0917bc7a2764
Stephen Fegan <sfegan@gmail.com>
2021-05-03 15:23:03 +0200

    Add secondary and camera params

--- src/simulation/sct_ray_tracer.cpp
+++ src/simulation/sct_ray_tracer.cpp
@@ -25,6 +25,7 @@
 #include <stdexcept>
 
 #include <util/log.hpp>
+#include <math/least_squares.hpp>
 #include <math/geometry.hpp>
 #include <math/vector3d_util.hpp>
 #include <simulation/sct_facet_scheme.hpp>
@@ -61,12 +62,21 @@ calin::simulation::sct_optics::make_sct_telescope(
   telescope->mutable_position()->set_y(y);
   telescope->mutable_position()->set_z(z);
 
+  // ***************************************************************************
+  // ***************************************************************************
+  //
+  // PRIMARY MIRROR SURFACE AND FACETS
+  //
+  // ***************************************************************************
+  // ***************************************************************************
+
   for(unsigned i=0;i<param.primary_sag_polynomial_size();++i) {
     double p_i = param.primary_sag_polynomial(i);
     telescope->add_primary_surface_polynomial(p_i);
   }
+
   Eigen::Vector3d primary_offset =
-    calin::math::vector3d_util::from_proto(telescope->primary_offset());
+    calin::math::vector3d_util::from_proto(param.primary_offset());
   if(param.primary_offset_xz_dispersion() > 0) {
     primary_offset.x() += param.primary_offset_xz_dispersion()*rng->normal();
     primary_offset.z() += param.primary_offset_xz_dispersion()*rng->normal();
@@ -100,23 +110,25 @@ calin::simulation::sct_optics::make_sct_telescope(
   for(unsigned ifacet=0; ifacet<primary_facet_scheme.num_facets(); ++ifacet) {
     auto* facet = telescope->add_primary_facets();
     facet->set_id(ifacet);
-    Eigen::Vector3d facet_offset(0,0,0);
+    Eigen::Vector3d facet_position(0,0,0);
+    primary_facet_scheme.facet_centroid(ifacet, facet_position.x(), facet_position.z());
+    facet_position.y() = calin::math::least_squares::polyval(
+      telescope->primary_surface_polynomial().data(),
+      telescope->primary_surface_polynomial_size(), facet_position.squaredNorm());
     if(param.primary_facet_offset_xz_dispersion() > 0) {
-      facet_offset.x() += param.primary_facet_offset_xz_dispersion()*rng->normal();
-      facet_offset.z() += param.primary_facet_offset_xz_dispersion()*rng->normal();
-      calin::math::vector3d_util::dump_as_proto(facet_offset, facet->mutable_offset());
+      facet_position.x() += param.primary_facet_offset_xz_dispersion()*rng->normal();
+      facet_position.z() += param.primary_facet_offset_xz_dispersion()*rng->normal();
     }
     if(param.primary_facet_offset_y_dispersion() > 0) {
-      facet_offset.y() += param.primary_facet_offset_y_dispersion()*rng->normal();
-      calin::math::vector3d_util::dump_as_proto(facet_offset, facet->mutable_offset());
+      facet_position.y() += param.primary_facet_offset_y_dispersion()*rng->normal();
     }
+    calin::math::vector3d_util::dump_as_proto(facet_position, facet->mutable_position());
     if(param.primary_facet_rotation_dispersion() > 0) {
       auto* facet_euler = facet->mutable_rotation();
       facet_euler->set_rotation_order(calin::ix::common_types::EulerAngles3D::XYX);
       calin::math::geometry::scattering_euler(facet_euler,
         param.primary_facet_rotation_dispersion(), *rng);
     }
-
     if(param.primary_facet_spot_size_mean()>0
         and param.primary_facet_spot_size_dispersion()>0) {
       facet->set_spot_size(rng->gamma_by_mean_and_sigma(
@@ -131,44 +143,135 @@ calin::simulation::sct_optics::make_sct_telescope(
     }
   }
 
-/*
-  message SCTFacet
-  {
-    uint32 id                                                = 1
-      [(CFO).desc = "Sequential facet identification number, starting at zero."];
-    bool removed                                             = 2
-      [(CFO).desc = "True if this mirror facet has been removed."];
-    double spot_size                                         = 3 [
-      (CFO).desc = "Parameter describing spot size from this mirror in "
-        "perfectly aligned system.", (CFO).units="cm" ];
-    calin.ix.common_types.Vector3D offset                    = 4 [
-      (CFO).desc = "Facet offset, if needed.", (CFO).units="cm" ];
-    calin.ix.common_types.EulerAngles3D rotation             = 5 [
-      (CFO).desc = "Facet rotation angles, if needed." ];
-  };
-*/
-
-  // calin.ix.common_types.Matrix3D primary_rotation          = 12 [
-  //   (CFO).desc = "Primary rotation matrix, if needed." ];
-  // SCTFacetScheme primary_facet_scheme                      = 13 [
-  //   (CFO).desc = "Primary facet scheme." ];
-  // repeated SCTFacet primary_facets                         = 14 [
-  //   (CFO).desc = "Primary facet parameters." ];
+  // ***************************************************************************
+  // ***************************************************************************
   //
+  // SECONDARY MIRROR SURFACE AND FACETS
   //
-  // repeated double primary_surface_polynomial               = 10 [
-  //   (CFO).desc = "Coefficients describing primary surface sag. Coefficients "
-  //     "are function of squared-distance from optic axis in cm.",
-  //   (CFO).units = "cm" ];
-  // calin.ix.common_types.Vector3D primary_offset            = 11 [
-  //   (CFO).desc = "Primary origin in reflector frame, if needed.", (CFO).units="cm" ];
-  // calin.ix.common_types.Matrix3D primary_rotation          = 12 [
-  //   (CFO).desc = "Primary rotation matrix, if needed." ];
-  // SCTFacetScheme primary_facet_scheme                      = 13 [
-  //   (CFO).desc = "Primary facet scheme." ];
-  // repeated SCTFacet primary_facets                         = 14 [
-  //   (CFO).desc = "Primary facet parameters." ];
+  // ***************************************************************************
+  // ***************************************************************************
 
+  for(unsigned i=0;i<param.secondary_sag_polynomial_size();++i) {
+    double p_i = param.secondary_sag_polynomial(i);
+    if(i == 0) {
+      p_i += param.secondary_distance();
+    }
+    telescope->add_secondary_surface_polynomial(p_i);
+  }
+
+  Eigen::Vector3d secondary_offset =
+    calin::math::vector3d_util::from_proto(param.secondary_offset());
+  if(param.secondary_offset_xz_dispersion() > 0) {
+    secondary_offset.x() += param.secondary_offset_xz_dispersion()*rng->normal();
+    secondary_offset.z() += param.secondary_offset_xz_dispersion()*rng->normal();
+  }
+  if(param.secondary_offset_y_dispersion() > 0) {
+    secondary_offset.y() += param.secondary_offset_y_dispersion()*rng->normal();
+  }
+  if(secondary_offset.squaredNorm() > 0) {
+    calin::math::vector3d_util::dump_as_proto(secondary_offset,
+      telescope->mutable_secondary_offset());
+  }
+
+  Eigen::Quaterniond secondary_rotation =
+    calin::math::geometry::euler_to_quaternion(param.secondary_rotation());
+  if(param.secondary_rotation_dispersion() >= 0) {
+    secondary_rotation =
+      calin::math::geometry::euler_to_quaternion(
+        calin::math::geometry::scattering_euler(param.secondary_rotation_dispersion(),
+          *rng, calin::ix::common_types::EulerAngles3D::XYX)) * secondary_rotation;
+  }
+  if(secondary_rotation.vec().squaredNorm() > 0) {
+    telescope->mutable_secondary_rotation()->set_rotation_order(
+      calin::ix::common_types::EulerAngles3D::XYX);
+    calin::math::geometry::quaternion_to_euler(
+      telescope->mutable_secondary_rotation(), secondary_rotation);
+  }
+  telescope->mutable_secondary_facet_scheme()->CopyFrom(param.secondary_facet_scheme());
+
+  auto secondary_facet_scheme = calin::simulation::sct_optics::SCTSecondaryFacetScheme(
+    param.secondary_facet_scheme());
+  for(unsigned ifacet=0; ifacet<secondary_facet_scheme.num_facets(); ++ifacet) {
+    auto* facet = telescope->add_secondary_facets();
+    facet->set_id(ifacet);
+    Eigen::Vector3d facet_position(0,0,0);
+    secondary_facet_scheme.facet_centroid(ifacet, facet_position.x(), facet_position.z());
+    facet_position.y() = calin::math::least_squares::polyval(
+      telescope->secondary_surface_polynomial().data(),
+      telescope->secondary_surface_polynomial_size(), facet_position.squaredNorm());
+    if(param.secondary_facet_offset_xz_dispersion() > 0) {
+      facet_position.x() += param.secondary_facet_offset_xz_dispersion()*rng->normal();
+      facet_position.z() += param.secondary_facet_offset_xz_dispersion()*rng->normal();
+    }
+    if(param.secondary_facet_offset_y_dispersion() > 0) {
+      facet_position.y() += param.secondary_facet_offset_y_dispersion()*rng->normal();
+    }
+    calin::math::vector3d_util::dump_as_proto(facet_position, facet->mutable_position());
+    if(param.secondary_facet_rotation_dispersion() > 0) {
+      auto* facet_euler = facet->mutable_rotation();
+      facet_euler->set_rotation_order(calin::ix::common_types::EulerAngles3D::XYX);
+      calin::math::geometry::scattering_euler(facet_euler,
+        param.secondary_facet_rotation_dispersion(), *rng);
+    }
+
+    if(param.secondary_facet_spot_size_mean()>0
+        and param.secondary_facet_spot_size_dispersion()>0) {
+      facet->set_spot_size(rng->gamma_by_mean_and_sigma(
+        param.secondary_facet_spot_size_mean(), param.secondary_facet_spot_size_dispersion()));
+    } else if(param.secondary_facet_spot_size_mean()>0) {
+      facet->set_spot_size(param.secondary_facet_spot_size_mean());
+    }
+  }
+  for(auto id : param.secondary_facets_removed()) {
+    if(id < secondary_facet_scheme.num_facets()) {
+      telescope->mutable_secondary_facets(id)->set_removed(true);
+    }
+  }
+
+  // ***************************************************************************
+  // ***************************************************************************
+  //
+  // CAMERA SURFACE AND MODULES
+  //
+  // ***************************************************************************
+  // ***************************************************************************
+
+  for(unsigned i=0;i<param.camera_sag_polynomial_size();++i) {
+    double p_i = param.camera_sag_polynomial(i);
+    if(i == 0) {
+      p_i += param.camera_distance();
+    }
+    telescope->add_camera_surface_polynomial(p_i);
+  }
+
+  Eigen::Vector3d camera_offset =
+    calin::math::vector3d_util::from_proto(param.camera_offset());
+  if(param.camera_offset_xz_dispersion() > 0) {
+    camera_offset.x() += param.camera_offset_xz_dispersion()*rng->normal();
+    camera_offset.z() += param.camera_offset_xz_dispersion()*rng->normal();
+  }
+  if(param.camera_offset_y_dispersion() > 0) {
+    camera_offset.y() += param.camera_offset_y_dispersion()*rng->normal();
+  }
+  if(camera_offset.squaredNorm() > 0) {
+    calin::math::vector3d_util::dump_as_proto(camera_offset,
+      telescope->mutable_camera_offset());
+  }
+
+  Eigen::Quaterniond camera_rotation =
+    calin::math::geometry::euler_to_quaternion(param.camera_rotation());
+  if(param.camera_rotation_dispersion() >= 0) {
+    camera_rotation =
+      calin::math::geometry::euler_to_quaternion(
+        calin::math::geometry::scattering_euler(param.camera_rotation_dispersion(),
+          *rng, calin::ix::common_types::EulerAngles3D::XYX)) * camera_rotation;
+  }
+  if(camera_rotation.vec().squaredNorm() > 0) {
+    telescope->mutable_camera_rotation()->set_rotation_order(
+      calin::ix::common_types::EulerAngles3D::XYX);
+    calin::math::geometry::quaternion_to_euler(
+      telescope->mutable_camera_rotation(), camera_rotation);
+  }
 
   return telescope;
 }
