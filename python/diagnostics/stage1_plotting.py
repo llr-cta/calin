8bbe66b26e4b01c7063b4581da9d14f3c3f64b17
Stephen Fegan <sfegan@gmail.com>
2021-06-29 10:17:32 +0200

    Add FEB temperature minmax plot

--- python/diagnostics/stage1_plotting.py
+++ python/diagnostics/stage1_plotting.py
@@ -35,17 +35,8 @@ def draw_missing_components_fraction(stage1, cmap = matplotlib.cm.CMRmap_r, axis
     cl = rc.const_camera_layout()
     ri = stage1.const_run_info()
 
-    xmin = numpy.min(cl.outline_polygon_vertex_x())
-    xmax = numpy.max(cl.outline_polygon_vertex_x())
-    ymin = numpy.min(cl.outline_polygon_vertex_y())
-    ymax = numpy.max(cl.outline_polygon_vertex_y())
-
-    # Delete these if you want the additional hardware to fit within the
-    # camera rectangle, rather than the square
-    xmin = min(xmin,ymin)
-    ymin = xmin
-    xmax = max(xmax,ymax)
-    ymax = xmax
+    max_xy = max(numpy.max(numpy.abs(cl.outline_polygon_vertex_x())),
+                 numpy.max(numpy.abs(cl.outline_polygon_vertex_y())))
 
     evts_ondisk    = int(ri.num_events_found())
     evts_maxid     = int(ri.event_numbers_found().end_index()[-1])
@@ -62,15 +53,15 @@ def draw_missing_components_fraction(stage1, cmap = matplotlib.cm.CMRmap_r, axis
 
     polygon_name = [ 'tib', 'cdts', 'both', 'lost\nevent' ]
 
-    rad = 10
+    rad = 10/120*max_xy
     additional_polygons.append(
-        matplotlib.patches.RegularPolygon(xy=(xmin+rad,ymax-rad*SQRT3_2),numVertices=6, radius=rad, orientation=RAD_30))
+        matplotlib.patches.RegularPolygon(xy=(-max_xy+rad,max_xy-rad*SQRT3_2),numVertices=6, radius=rad, orientation=RAD_30))
     additional_polygons.append(
-        matplotlib.patches.RegularPolygon(xy=(xmin+rad,ymax-rad*SQRT3_2-2*rad),numVertices=6, radius=rad, orientation=RAD_30))
+        matplotlib.patches.RegularPolygon(xy=(-max_xy+rad,max_xy-rad*SQRT3_2-2*rad),numVertices=6, radius=rad, orientation=RAD_30))
     additional_polygons.append(
-        matplotlib.patches.RegularPolygon(xy=(xmin+rad+2*rad*SQRT3_2,ymax-rad*SQRT3_2-rad),numVertices=6, radius=rad, orientation=RAD_30))
+        matplotlib.patches.RegularPolygon(xy=(-max_xy+rad+2*rad*SQRT3_2,max_xy-rad*SQRT3_2-rad),numVertices=6, radius=rad, orientation=RAD_30))
     additional_polygons.append(
-        matplotlib.patches.Circle(xy=(xmax-1.5*rad,ymin+1.5*rad), radius=1.5*rad))
+        matplotlib.patches.Circle(xy=(max_xy-1.5*rad,-max_xy+1.5*rad), radius=1.5*rad))
 
     if(axis is None):
         axis = matplotlib.pyplot.gca()
@@ -113,22 +104,22 @@ def draw_missing_components_fraction(stage1, cmap = matplotlib.cm.CMRmap_r, axis
             fontsize=aux_label_fontsize, color=label_color(additional_polygon_data[ip]))
 
     if(stat_label_fontsize):
-        axis.text(xmin,ymin,
+        axis.text(-max_xy,-max_xy,
             f'Event counts\nOn disk:   {evts_ondisk:,}\nTriggered: {evts_maxid-1:,}\nDuplicate: {evts_duplicate:,}',
             ha='left', va='bottom', fontfamily='monospace',
             fontsize=stat_label_fontsize);
 
     pc.set_norm(matplotlib.colors.LogNorm(vmin=vmin,vmax=1.0))
-    axis.axis(numpy.asarray([-1,1,-1,1])*1.05*max([ymax,xmax,abs(xmin),abs(ymin)]))
+    axis.axis(numpy.asarray([-1,1,-1,1])*1.05*max_xy)
     axis.get_xaxis().set_visible(False)
     axis.get_yaxis().set_visible(False)
 
     cb = axis.get_figure().colorbar(pc, ax=axis, label='Component missing fraction')
-    cb.ax.plot([xmin, xmax], [1.0/evts_ondisk, 1.0/evts_ondisk], 'g-', lw=0.75) # my data is between 0 and 1
+    cb.ax.plot([-max_xy, max_xy], [1.0/evts_ondisk, 1.0/evts_ondisk], 'g-', lw=0.75)
 
     return pc
 
-def draw_feb_temperatures(stage1, temperature_set=1, cmap = matplotlib.cm.CMRmap, axis = None,
+def draw_nectarcam_feb_temperatures(stage1, temperature_set=1, cmap = matplotlib.cm.CMRmap, axis = None,
         draw_outline = True, mod_lw = 0, outline_lw = 0.5, outline_color = '#888888',
         mod_label_fontsize=4, stat_label_fontsize=4.75):
     tfeb1 = []
@@ -163,6 +154,78 @@ def draw_feb_temperatures(stage1, temperature_set=1, cmap = matplotlib.cm.CMRmap
         m = numpy.asarray(m)
         return ' ====' if(numpy.all(m==0) or numpy.any(m<-30) or numpy.any(m>50)) else \
             ' %4.1f'%numpy.mean(m)
+
+    if(stage1.nectarcam().ancillary_data().has_ecc_measurements() and \
+            stage1.nectarcam().ancillary_data().ecc_measurements().measurement_size()>0):
+        measurement_set = stage1.nectarcam().ancillary_data().ecc_measurements()
+
+        tecc = 'ECC'
+        tecc += measurement_val([measurement_set.measurement(i).temp_01() for i in range(measurement_set.measurement_size())])
+        tecc += measurement_val([measurement_set.measurement(i).temp_02() for i in range(measurement_set.measurement_size())])
+        tecc += measurement_val([measurement_set.measurement(i).temp_03() for i in range(measurement_set.measurement_size())])
+        tecc += measurement_val([measurement_set.measurement(i).temp_04() for i in range(measurement_set.measurement_size())])
+        tecc += '\n\u00b0C'
+        tecc += measurement_val([measurement_set.measurement(i).temp_05() for i in range(measurement_set.measurement_size())])
+        tecc += measurement_val([measurement_set.measurement(i).temp_06() for i in range(measurement_set.measurement_size())])
+        tecc += measurement_val([measurement_set.measurement(i).temp_07() for i in range(measurement_set.measurement_size())])
+        tecc += measurement_val([measurement_set.measurement(i).temp_08() for i in range(measurement_set.measurement_size())])
+        tecc += '\n'
+        tecc += measurement_val([measurement_set.measurement(i).temp_09() for i in range(measurement_set.measurement_size())])[1:]
+        tecc += measurement_val([measurement_set.measurement(i).temp_10() for i in range(measurement_set.measurement_size())])
+        tecc += measurement_val([measurement_set.measurement(i).temp_11() for i in range(measurement_set.measurement_size())])
+        tecc += measurement_val([measurement_set.measurement(i).temp_12() for i in range(measurement_set.measurement_size())])
+
+        tecc4 = measurement_val([measurement_set.measurement(i).temp_13() for i in range(measurement_set.measurement_size())])[1:]
+        tecc4 += measurement_val([measurement_set.measurement(i).temp_14() for i in range(measurement_set.measurement_size())])
+        tecc4 += measurement_val([measurement_set.measurement(i).temp_15() for i in range(measurement_set.measurement_size())])
+        tecc4 += measurement_val([measurement_set.measurement(i).temp_16() for i in range(measurement_set.measurement_size())])
+
+        if(tecc4 != '==== ==== ==== ===='):
+            tecc += '\n' + tecc4
+
+        max_xy = max(numpy.max(numpy.abs(stage1.run_config().camera_layout().outline_polygon_vertex_x())),
+                          numpy.max(numpy.abs(stage1.run_config().camera_layout().outline_polygon_vertex_y())))
+        axis.text(max_xy,max_xy,tecc,ha='right',va='top',fontfamily='monospace',fontsize=stat_label_fontsize)
+
+def draw_nectarcam_feb_temperatures_minmax(stage1, temperature_set=1, cmap = matplotlib.cm.CMRmap, axis = None,
+        draw_outline = True, mod_lw = 0, outline_lw = 0.5, outline_color = '#888888',
+        mod_label_fontsize=4, stat_label_fontsize=4.75):
+
+    def minmax(x):
+        return numpy.max(x)-numpy.min(x)
+
+    tfeb1 = []
+    tfeb2 = []
+    for modid in stage1.run_config().configured_module_id() :
+        if(stage1.nectarcam().ancillary_data().feb_temperature_has_key(int(modid))):
+            measurement_set = stage1.nectarcam().ancillary_data().feb_temperature(int(modid))
+            tfeb1.append(minmax([measurement_set.measurement(i).tfeb1() for i in range(measurement_set.measurement_size())]))
+            tfeb2.append(minmax([measurement_set.measurement(i).tfeb2() for i in range(measurement_set.measurement_size())]))
+    tfeb = tfeb1 if temperature_set==1 else tfeb2
+
+    if(axis is None):
+        axis = matplotlib.pyplot.gca()
+
+    pc = calin.plotting.plot_camera_module_image(tfeb, stage1.run_config().camera_layout(),
+                    configured_modules=stage1.run_config().configured_module_id(),
+                    axis=axis, cmap=cmap, draw_outline=True, draw_stats=True,
+                    mod_lw=mod_lw, outline_lw=outline_lw, outline_color=outline_color,
+                    hatch_missing_modules=True, stats_format=u'%4.2f\u00b0C',
+                    stats_fontsize=stat_label_fontsize)
+    cb = axis.get_figure().colorbar(pc, label=u'FEB temperature %d range (max-min) [\u2103]'%temperature_set)
+
+    if(mod_label_fontsize is not None and mod_label_fontsize>0):
+        calin.plotting.add_module_numbers(axis, stage1.run_config().camera_layout(),
+                                configured_modules=stage1.run_config().configured_module_id(),
+                                pc=pc, module_values = tfeb, fontsize=mod_label_fontsize)
+
+    axis.get_xaxis().set_visible(False)
+    axis.get_yaxis().set_visible(False)
+
+    def measurement_val(m):
+        m = numpy.asarray(m)
+        return ' ====' if(numpy.all(m==0) or numpy.any(m<-30) or numpy.any(m>50)) else \
+            ' %4.1f'%minmax(m)
 
     if(stage1.nectarcam().ancillary_data().has_ecc_measurements() and \
             stage1.nectarcam().ancillary_data().ecc_measurements().measurement_size()>0):
