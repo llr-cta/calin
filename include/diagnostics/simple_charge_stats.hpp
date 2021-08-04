9c32ddaa7b0714e61a24437dc6bed4419f1aa9bf
Stephen Fegan <sfegan@gmail.com>
2021-08-04 14:21:16 +0200

    Complete signed unsigned

--- include/diagnostics/simple_charge_stats.hpp
+++ include/diagnostics/simple_charge_stats.hpp
@@ -168,7 +168,7 @@ private:
     calin::ix::diagnostics::simple_charge_stats::PartialOneGainChannelSimpleChargeStats* one_gain_stats,
     SingleGainChannelHists* one_gain_hists,
     unsigned& nsum, int64_t& opt_sum, int64_t& sig_sum, int64_t& bkg_sum, int64_t& wf_sum,
-     unsigned wf_clipping_value);
+    int wf_clipping_value);
 
   void record_one_visitor_data(uint64_t seq_index, const calin::ix::iact_data::telescope_event::TelescopeEvent* event,
     const calin::iact_data::waveform_treatment_event_visitor::OptimalWindowSumWaveformTreatmentParallelEventVisitor* sum_visitor,
