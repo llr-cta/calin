56300756dcb259c445d49dab5fe599236302a265
Stephen Fegan <sfegan@gmail.com>
2021-09-14 14:55:43 +0200

    New attempt to get git branch

--- tools/get_git_branch.sh
+++ tools/get_git_branch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # See https://stackoverflow.com/questions/6059336/how-to-find-the-current-git-branch-in-detached-head-state
-$1 for-each-ref --format='%(objectname) %(refname:short)' refs/heads | awk "/^$($1 rev-parse HEAD)/ {print \$2}"
+$1 git describe --contains --all HEAD | rev | cut -d/ -f1 | rev
