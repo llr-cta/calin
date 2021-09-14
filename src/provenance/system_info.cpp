060a1bb2e555f476298cd43e147f8a41e5883001
Stephen Fegan <sfegan@gmail.com>
2021-09-14 14:55:19 +0200

    Don't print branch if not available

--- src/provenance/system_info.cpp
+++ src/provenance/system_info.cpp
@@ -365,7 +365,11 @@ std::string calin::provenance::system_info::build_info_string(
     build_info = calin::provenance::system_info::the_build_info();
   }
   std::ostringstream L;
-  L << "Git origin : " << build_info->git_origin_url() << "  Branch : " << build_info->git_branch() << "\n"
+  L << "Git origin : " << build_info->git_origin_url();
+  if(not build_info->git_branch().empty()) {
+    L << "  Branch : " << build_info->git_branch();
+  }
+  L << '\n'
     << "Git commit : " << build_info->git_commit_sha1() << " from " << build_info->git_commit_date();
   if(build_info->git_repo_status() == "dirty") {
     L << " (dirty)";
