385f59c2546ec4f75534d06cf8bcf6160f680f70
Stephen Fegan <sfegan@gmail.com>
2022-07-04 13:56:05 +0200

    Continue implementing (not tested)

--- include/simulation/vcl_iact_array.hpp
+++ include/simulation/vcl_iact_array.hpp
@@ -60,16 +60,22 @@ public:
   CALIN_TYPEALIAS(VCLFocalPlaneRayPropagator, calin::simulation::vcl_ray_propagator::VCLFocalPlaneRayPropagator<VCLArchitecture>);
 
   VCLIACTArray(calin::simulation::atmosphere::LayeredRefractiveAtmosphere* atm,
-    const calin::ix::simulation::vcl_iact::VCLIACTConfiguration& config = VCLIACTTrackVisitor<VCLArchitecture>::default_config(),
+    const calin::ix::simulation::vcl_iact::VCLIACTArrayConfiguration& config = default_config(),
     calin::math::rng::VCLRNG<VCLArchitecture>* rng = nullptr,
     bool adopt_atm = false, bool adopt_rng = false);
   virtual ~VCLIACTArray();
+
+  static calin::ix::simulation::vcl_iact::VCLIACTArrayConfiguration default_config();
 
 #ifndef SWIG
   void visit_event(const calin::simulation::tracker::Event& event, bool& kill_event) final;
   void propagate_rays(calin::math::ray::VCLRay<double_real> ray, double_bvt ray_mask, double_vt ray_weight) final;
   void leave_event() final;
+
 protected:
+  static calin::ix::simulation::vcl_iact::VCLIACTConfiguration base_config(
+    const calin::ix::simulation::vcl_iact::VCLIACTArrayConfiguration& config);
+
   struct PropagatorInfo {
     VCLFocalPlaneRayPropagator* propagator;
     unsigned detector0;
@@ -77,32 +83,42 @@ protected:
   };
   struct DetectorInfo {
     RayProcessorDetectorSphere sphere;
+    double squared_radius;
     VCLFocalPlaneRayPropagator* propagator;
     unsigned propagator_iscope;
-    unsigned nray;
-    VCLRayArray ray;
-    double_at ray_weight;
+    unsigned global_iscope;
+    double ref_index;
+
+    VCLRayArray rays_to_refract;
+    unsigned nrays_to_refract;
+    double_at ray_weights_to_refract;
+
+    VCLRayArray rays_to_propagate;
+    unsigned nrays_to_propagate;
+    double_at ray_weights_to_propagate;
   };
-  std::vector<PropagatorInfo> propoagator;
-  std::vector<DetectorInfo> detector;
+  std::vector<PropagatorInfo> propoagator_;
+  std::vector<DetectorInfo> detector_;
+  calin::ix::simulation::vcl_iact::VCLIACTArrayConfiguration config_;
 #endif
 };
 
 #ifndef SWIG
 
 template<typename VCLArchitecture> VCLIACTArray<VCLArchitecture>::
-VCLIACTGroundMap(
+VCLIACTArray(
     calin::simulation::atmosphere::LayeredRefractiveAtmosphere* atm,
-    const calin::ix::simulation::vcl_iact::VCLIACTConfiguration& config,
+    const calin::ix::simulation::vcl_iact::VCLIACTArrayConfiguration& config,
     calin::math::rng::VCLRNG<VCLArchitecture>* rng,
     bool adopt_atm, bool adopt_rng):
-  VCLIACTTrackVisitor<VCLArchitecture>(atm, config, rng, adopt_atm, adopt_rng)
+  VCLIACTTrackVisitor<VCLArchitecture>(atm, base_config(config), rng, adopt_atm, adopt_rng),
+  config_(config)
 {
   // nothing to see here
 }
 
 template<typename VCLArchitecture> VCLIACTGroundMap<VCLArchitecture>::
-~VCLIACTGroundMap()
+~VCLIACTArray()
 {
   // nothing to see here
 }
@@ -118,19 +134,76 @@ visit_event(const calin::simulation::tracker::Event& event, bool& kill_event)
     }
     for(unsigned isphere=0; isphere<spheres.size(); ++isphere) {
       detector[ipropoagator.detector0+isphere].sphere = spheres[isphere];
-      detector[ipropoagator.detector0+isphere].nray = 0;
+      detector[ipropoagator.detector0+isphere].nrays_to_refract = 0;
+      detector[ipropoagator.detector0+isphere].nrays_to_propagate = 0;
     }
   }
 
   return VCLIACTTrackVisitor<VCLArchitecture>::visit_event(event, kill_event);
 }
 
-template<typename VCLArchitecture> void VCLIACTGroundMap<VCLArchitecture>::
+template<typename VCLArchitecture> void VCLIACTArray<VCLArchitecture>::
 propagate_rays(calin::math::ray::VCLRay<double_real> ray, double_bvt ray_mask, double_vt ray_weight)
 {
+  switch(config_.refraction_mode()){
+  case calin::ix::simulation::vcl_iact::REFRACT_NO_RAYS:
+    break;
+  case calin::ix::simulation::vcl_iact::REFRACT_ALL_RAYS:
+    break;
+  case calin::ix::simulation::vcl_iact::REFTACT_ONLY_CLOSE_RAYS:
+    break;
+  }
+
+  calin::math::ray::VCLRayArray<double_real> ray_array { ray };
+  double_at ray_weight_array
+  ray_weight.store(ray_weight_array);
+
+  for(auto& idetector : detector_) {
+    auto intersecting_rays = ray_mask & (ray.squared_distance_at_closest_approach() < detector.squared_radius);
+    unsigned intersecting_rays_bitmask = vcl::to_bits(intersecting_rays);
+    if(intersecting_rays_bitmask) {
+      calin::math::ray::VCLRayArray<double_real> ray_array(ray);
+      for(unsigned iray=0; iray<VCLArchitecture::num_double; ++iray) {
+        if(intersecting_rays_bitmask & 1) {
+          idetector.rays_to_propagate.insert(detector.nrays_to_propagate, ray_array.extract(iray));
+          idetector.ray_weights_to_propagate[detector.nrays_to_propagate] = ray_weight_array[iray];
+          ++idetector.nrays_to_propagate;
+          if(idetector.nrays_to_propagate == VCLArchitecture::num_double) {
+            do_propagate_rays_for_detector(idetector)
+          }
+        }
+        intersecting_rays_bitmask >>= 1;
+      }
+    }
+  }
+
   ray_mask = ray.propagate_to_z_plane_with_mask(ray_mask,
     VCLIACTTrackVisitor<VCLArchitecture>::atm_->zobs(0), false);
-  
+
+}
+
+template<typename VCLArchitecture> void VCLIACTArray<VCLArchitecture>::
+do_propagate_rays_for_detector(DetectorInfo& idetector)
+{
+  idetector.nrays_to_propagate = 0;
+}
+
+template<typename VCLArchitecture> calin::ix::simulation::vcl_iact::VCLIACTArrayConfiguration
+VCLIACTArray<VCLArchitecture>::default_config()
+{
+  calin::ix::simulation::vcl_iact::VCLIACTArrayConfiguration config;
+  config.set_bandwidth(3.0);
+  return config;
+}
+
+template<typename VCLArchitecture> calin::ix::simulation::vcl_iact::VCLIACTConfiguration
+VCLIACTArray<VCLArchitecture>::base_config(const calin::ix::simulation::vcl_iact::VCLIACTArrayConfiguration& config)
+{
+  calin::ix::simulation::vcl_iact::VCLIACTConfiguration bconfig;
+  bconfig.set_bandwidth(config.bandwidth());
+  bconfig.set_enable_forced_cherenkov_angle_mode(config.enable_forced_cherenkov_angle_mode());
+  bconfig.set_forced_cherenkov_angle(config.forced_cherenkov_angle());
+  return bconfig;
 }
 
 #endif // not defined SWIG
