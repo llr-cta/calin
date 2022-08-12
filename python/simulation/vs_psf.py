4cb653302b174bdd386f17152d3209c4d85aaa79
Stephen Fegan <sfegan@gmail.com>
2022-08-12 18:40:24 +0100

    Add D80 calculation

--- python/simulation/vs_psf.py
+++ python/simulation/vs_psf.py
@@ -14,9 +14,10 @@ import calin.simulation.ray_processor
 import calin.simulation.vs_optics
 import calin.simulation.vs_cta
 import calin.math.geometry
+import scipy.optimize
 import calin.provenance.system_info
 
-def calc_psf(vs_scope, theta_deg, phi_deg, refocus_at_infinity = False, dx=0.1, fp_resolution=0.1):
+def calc_psf(vs_scope, theta_deg, phi_deg, refocus_at_infinity = False, dx=0.1, image_resolution=0.01, d80_frac=0.8):
     scope_cfg = vs_scope.dump_as_proto()
 #     print(scope_cfg.DebugString())
     if(refocus_at_infinity):
@@ -31,7 +32,7 @@ def calc_psf(vs_scope, theta_deg, phi_deg, refocus_at_infinity = False, dx=0.1, 
     ns_cfg = calin.simulation.ray_processor.NSpacePEProcessor.default_config()
     ns_cfg.set_axis_variables(calin.ix.simulation.ray_processor.XY)
     ns_cfg.set_xy_diameter(max(scope_cfg.camera_diameter(), 2*theta_deg*scope_cfg.fp_translation().y()/180*numpy.pi)*1.2)
-    ns_cfg.set_xy_num_bins(int(ns_cfg.xy_diameter()/fp_resolution))
+    ns_cfg.set_xy_num_bins(int(ns_cfg.xy_diameter()/(image_resolution*scope.pixelSpacing())))
 
     pe_imager = calin.simulation.ray_processor.NSpacePEProcessor(ns_cfg)
 
@@ -59,7 +60,42 @@ def calc_psf(vs_scope, theta_deg, phi_deg, refocus_at_infinity = False, dx=0.1, 
     nhit = processor.nhit()
 
     ns = pe_imager.nspace()
-
     m2,m1,m0 = ns.covar_mean_and_total_weight()
 
-    return m0,m1,m2,nray,nhit
+    xc = ns.axis_bin_centers(0)
+    yc = ns.axis_bin_centers(1)
+    ns_vals = ns.as_matrix().transpose()
+
+    ns_vals_px = numpy.sum(ns_vals, axis=0)
+    ix0 = numpy.min(numpy.where(ns_vals_px>0))
+    ix1 = numpy.max(numpy.where(ns_vals_px>0))
+
+    ns_vals_py = numpy.sum(ns_vals, axis=1)
+    iy0 = numpy.min(numpy.where(ns_vals_py>0))
+    iy1 = numpy.max(numpy.where(ns_vals_py>0))
+
+    xc = xc[ix0:ix1+1]
+    yc = yc[iy0:iy1+1]
+    Xc, Yc = numpy.meshgrid(xc,yc)
+
+    ns_vals = ns_vals[iy0:iy1+1,ix0:ix1+1]
+    ns_vals = ns_vals.flatten()
+
+    def d80_for_xy(xy0):
+        x0,y0 = xy0
+
+        X = Xc - x0
+        Y = Yc - y0
+
+        d80x = numpy.sqrt(X.flatten()**2 + Y.flatten()**2)
+        isort = numpy.argsort(d80x)
+        d80x = d80x[isort]
+        d80y = numpy.cumsum(ns_vals[isort])/numpy.sum(ns_vals)
+
+        d80 = numpy.interp(d80_frac, d80y, d80x)
+
+        return d80
+
+    opt = scipy.optimize.minimize(d80_for_xy, m1, method='Nelder-Mead')
+
+    return m0,m1,m2,opt.fun*2,opt.x,nray,nhit
