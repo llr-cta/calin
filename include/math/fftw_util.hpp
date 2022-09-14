892945d36f00d4b8ec3418eeec51dca0f09dfecf
Stephen Fegan <sfegan@gmail.com>
2022-09-14 13:54:59 +0200

    Add VCL version of hcvec_scale_and_multiply

--- include/math/fftw_util.hpp
+++ include/math/fftw_util.hpp
@@ -91,6 +91,103 @@ void hcvec_scale_and_multiply(T* ovec, const T* ivec1,
   if(ro==co)(*ro) = (*ri1) * (*ri2) * scale;
 }
 
+template<typename VCLReal>
+void hcvec_scale_and_multiply_vcl(typename VCLReal::real_t* ovec,
+  const typename VCLReal::real_t* ivec1, const typename VCLReal::real_t* ivec2,
+  unsigned nsample, typename VCLReal::real_t scale = 1.0)
+{
+  typename VCLReal::real_t* ro = ovec;
+  typename VCLReal::real_t* co = ovec + nsample;
+  const typename VCLReal::real_t* ri1 = ivec1;
+  const typename VCLReal::real_t* ci1 = ivec1 + nsample;
+  const typename VCLReal::real_t* ri2 = ivec2;
+  const typename VCLReal::real_t* ci2 = ivec2 + nsample;
+
+  (*ro++) = (*ri1++) * (*ri2++) * scale;
+
+  while(co - ro >= 2*VCLReal::num_real)
+  {
+    co -= VCLReal::num_real;
+    ci1 -= VCLReal::num_real;
+    ci2 -= VCLReal::num_real;
+    typename VCLReal::real_vt vri1; vri1.load(ri1);
+    typename VCLReal::real_vt vci1; vci1.load(ci1);
+    typename VCLReal::real_vt vri2; vri2.load(ri2);
+    typename VCLReal::real_vt vci2; vci2.load(ci2);
+
+    typename VCLReal::real_vt vro = (vri1*vri2 - calin::util::vcl::reverse(vci1*vci2)) * scale;
+    typename VCLReal::real_vt vco = (calin::util::vcl::reverse(vri1)*vci2 + vci1*calin::util::vcl::reverse(vri2)) * scale;
+
+    vro.store(ro);
+    vco.store(co);
+
+    ro += VCLReal::num_real;
+    ri1 += VCLReal::num_real;
+    ri2 += VCLReal::num_real;
+  }
+
+  --co;
+  --ci1;
+  --ci2;
+  while(ro < co)
+  {
+    double vri1 = *ri1++;
+    double vci1 = *ci1--;
+    double vri2 = *ri2++;
+    double vci2 = *ci2--;
+    (*ro++) = (vri1*vri2 - vci1*vci2) * scale;
+    (*co--) = (vri1*vci2 + vci1*vri2) * scale;
+  }
+  if(ro==co)(*ro) = (*ri1) * (*ri2) * scale;
+}
+// NOTE : This function overrides the template for systems with AVX !!!
+#if INSTRSET >= 7
+void hcvec_scale_and_multiply(double* ovec, const double* ivec1,
+  const double* ivec2, unsigned nsample, double scale = 1.0);
+#endif
+
+#if 0
+template<typename T>
+void hcvec_scale_and_multiply_with_stride(T* ovec, int ostride,
+  const T* ivec1, int istride1, const T* ivec2, int istride2, unsigned nsample, T scale = 1.0)
+{
+  T *ro = ovec;
+  T *co = ovec + (nsample-1)*ostride;
+  const T *ri1 = ivec1;
+  const T *ci1 = ivec1 + (nsample-1)*istride1;
+  const T *ri2 = ivec2;
+  const T *ci2 = ivec2 + (nsample-1)*istride2;
+
+  *ro = *ri1 * *ri2 * scale;
+  ro += ostride, ri1 += istride1, ri2 += istride2;
+  if(ro==ri1 or ro==ri2)
+  {
+    while(ro < co)
+    {
+      T vri1 = *ri1;
+      T vci1 = *ci1;
+      T vri2 = *ri2;
+      T vci2 = *ci2;
+      *ro = (vri1*vri2 - vci1*vci2)*scale;
+      *co = (vri1*vci2 + vci1*vri2)*scale;
+      ro += ostride, ri1 += istride1, ri2 += istride2;
+      co -= ostride, ci1 -= istride1, ci2 -= istride2;
+    }
+   }
+  else
+  {
+    while(ro < co)
+    {
+      *ro = ((*ri1)*(*ri2) - (*ci1)*(*ci2)) * scale;
+      *co = ((*ri1)*(*ci2) + (*ci1)*(*ri2)) * scale;
+      ro += ostride, ri1 += istride1, ri2 += istride2;
+      co -= ostride, ci1 -= istride1, ci2 -= istride2;
+    }
+  }
+  if(ro==co)(*ro) = (*ri1) * (*ri2) * scale;
+}
+#endif
+
 template<typename T>
 void hcvec_scale_and_multiply_conj(T* ovec, const T* ivec1,
   const T* ivec2_conj, unsigned nsample, T scale = 1.0)
