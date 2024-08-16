379a097fa96482f03b97552c9a8daf819dc50739
Stephen Fegan <sfegan@gmail.com>
2024-08-16 14:21:06 +0100

    Attempt to use upgraded cmake

--- configure_centos7_llrcta.sh
+++
@@ -1,8 +0,0 @@
-cmake3 -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-  -DPYTHON_EXECUTABLE=/usr/bin/python3.4 \
-  -DPYTHON_INCLUDE_DIR=/usr/include/python3.4m \
-  -DPYTHON_LIBRARY=/usr/lib64/libpython3.so \
-  -DCMAKE_PREFIX_PATH=/opt/exp_soft/vo.cta.in2p3.fr/local \
-  -DCMAKE_LIBRARY_PATH=/usr/lib64 \
-  -DCMAKE_INSTALL_PREFIX=$HOME/local \
-  ..
--- configure_macports_py36.sh
+++
@@ -1,7 +0,0 @@
-cmake \ #--graphviz=calin_deps.dot \
-    -DPYTHON_EXECUTABLE=/opt/local/bin/python3.6 \
-    -DPYTHON_INCLUDE_DIR=/opt/local/Library/Frameworks/Python.framework/Versions/3.6/include/python3.6m \
-    -DPYTHON_LIBRARY=/opt/local/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/config-3.6m-darwin/libpython3.6.dylib \
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-    -DCTA_CAMERASTOACT_DIR=$HOME/local \
-    -DCMAKE_INSTALL_PREFIX=$HOME/local ..
