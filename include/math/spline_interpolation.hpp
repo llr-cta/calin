29824cb74eed634e590d999e9b728c9a4edf9d4d
Stephen Fegan <sfegan@gmail.com>
2026-08-05 08:36:06 +0200

    Add vcl_real_value

--- include/math/spline_interpolation.hpp
+++ include/math/spline_interpolation.hpp
@@ -370,6 +370,9 @@ public:
   template<typename VCLArchitecture> inline typename VCLArchitecture::double_vt
   vcl_value(typename VCLArchitecture::double_vt x) const;
 
+  template<typename VCLReal> inline typename VCLReal::real_vt
+  vcl_real_value(typename VCLReal::real_vt x) const;
+
 private:
   void init();
   CubicSplineIntervals s_;
@@ -393,6 +396,25 @@ CubicSpline::vcl_value(typename VCLArchitecture::double_vt x) const
   double_vt dx_inv;
   int64_vt iinterval = vcl_find_interval<calin::util::vcl::VCLDoubleReal<VCLArchitecture> >(x, s_, x0, dx, dx_inv);
   double_vt t = (x-x0)*dx_inv;
+
+  return cubic_value(t, dx, dx_inv,
+    vcl::lookup<0x40000000>(iinterval, s_.y.data()),
+    vcl::lookup<0x40000000>(iinterval, s_.y.data()+1),
+    vcl::lookup<0x40000000>(iinterval, s_.dy_dx.data()),
+    vcl::lookup<0x40000000>(iinterval, s_.dy_dx.data()+1));
+}
+
+template<typename VCLReal> typename VCLReal::real_vt
+CubicSpline::vcl_real_value(typename VCLReal::real_vt x) const
+{
+  typedef typename VCLReal::int_vt int_vt;
+  typedef typename VCLReal::real_vt real_vt;
+
+  real_vt x0;
+  real_vt dx;
+  real_vt dx_inv;
+  int_vt iinterval = vcl_find_interval<VCLReal>(x, s_, x0, dx, dx_inv);
+  real_vt t = (x-x0)*dx_inv;
 
   return cubic_value(t, dx, dx_inv,
     vcl::lookup<0x40000000>(iinterval, s_.y.data()),
